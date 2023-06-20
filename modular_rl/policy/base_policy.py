9a758b05b3de12ee0ae5922f6dbab00f7187c5a9
Melvin Laux <melvin.laux@dfki.de>
2023-06-20 15:56:35 +0200

    added action_probability function

--- modular_rl/policy/base_policy.py
+++ modular_rl/policy/base_policy.py
@@ -14,12 +14,19 @@ class BasePolicy(abc.ABC):
     def get_action(self, observation: npt.ArrayLike) -> npt.ArrayLike:
         pass
 
+    @abc.abstractmethod
+    def get_action_probability(self, action: npt.ArrayLike, observation: npt.ArrayLike) -> float:
+        pass
+
 
 class UniformRandomPolicy(BasePolicy):
     """Simple Random Policy."""
 
     def get_action(self, observation: npt.ArrayLike) -> npt.ArrayLike:
         return self.action_space.sample()
+
+    def get_action_probability(self, action: npt.ArrayLike, observation: npt.ArrayLike) -> float:
+        return 1.0 / self.action_space.n
 
 
 class GreedyQPolicy(BasePolicy):
@@ -34,6 +41,9 @@ class GreedyQPolicy(BasePolicy):
 
     def get_action(self, observation: npt.ArrayLike) -> npt.ArrayLike:
         return np.random.choice(np.flatnonzero(self.q_table[observation] == self.q_table[observation].max()))
+
+    def get_action_probability(self, action: npt.ArrayLike, observation: npt.ArrayLike) -> float:
+        return 1.0 / np.count_nonzero(self.q_table[observation] == self.q_table[observation].max())
 
     def update(self, idx, value) -> None:
         self.q_table[idx] = value
