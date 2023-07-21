5be572349ed2d177579c698357b5603617b64f23
Melvin Laux <melvin.laux@dfki.de>
2023-07-21 17:52:02 +0200

    added epsilon greedy policy

--- modular_rl/policy/base_policy.py
+++ modular_rl/policy/base_policy.py
@@ -2,6 +2,9 @@ import abc
 import numpy as np
 import numpy.typing as npt
 
+from modular_rl.policy.value_functions import TabularValueFunction, TabularQFunction
+from numpy.random import default_rng
+
 
 class BasePolicy(abc.ABC):
     """Base policy class interface."""
@@ -19,6 +22,35 @@ class BasePolicy(abc.ABC):
         pass
 
 
+class StateValueBasedPolicy(BasePolicy):
+    """Base policy class for state-value-based policies."""
+
+    def __init__(self, observation_space, action_space):
+        super().__init__(observation_space, action_space)
+
+        self.value_function = TabularValueFunction(observation_space)
+
+
+class QValueBasedPolicy(BasePolicy):
+    """Base policy class for q-value-based policies."""
+
+    def __init__(self, observation_space, action_space):
+        super().__init__(observation_space, action_space)
+
+        self.value_function = TabularQFunction(observation_space, action_space)
+
+    @abc.abstractmethod
+    def get_action(self, observation: npt.ArrayLike) -> npt.ArrayLike:
+        pass
+
+    @abc.abstractmethod
+    def get_action_probability(self, action: npt.ArrayLike, observation: npt.ArrayLike) -> float:
+        pass
+
+    def update(self, observations, actions, values) -> None:
+        self.value_function.update(observations, actions, values)
+
+
 class UniformRandomPolicy(BasePolicy):
     """Simple Random Policy."""
 
@@ -29,21 +61,35 @@ class UniformRandomPolicy(BasePolicy):
         return 1.0 / self.action_space.n
 
 
-class GreedyQPolicy(BasePolicy):
+class GreedyQPolicy(QValueBasedPolicy):
     """
     Greedy policy that selects the action that maximises the Q-function.
     """
 
-    def __init__(self, observation_space, action_space, initial_value=0.0):
-        super().__init__(observation_space, action_space)
-
-        self.q_table = np.full(shape=(observation_space.n, action_space.n), fill_value=initial_value)
-
     def get_action(self, observation: npt.ArrayLike) -> npt.ArrayLike:
-        return np.random.choice(np.flatnonzero(self.q_table[observation] == self.q_table[observation].max()))
+        return default_rng(42).choice(np.flatnonzero(self.value_function.values[observation] == self.value_function.values[observation].max()))
 
     def get_action_probability(self, action: npt.ArrayLike, observation: npt.ArrayLike) -> float:
-        return 1.0 / np.count_nonzero(self.q_table[observation] == self.q_table[observation].max())
+        return 1.0 / np.count_nonzero(self.value_function.values[observation] == self.value_function.values[observation].max())
 
-    def update(self, idx, value) -> None:
-        self.q_table[idx] = value
+
+class EpsilonGreedyPolicy(QValueBasedPolicy):
+    """
+    Epsilon-Greedy policy that selects the action that mximises the Q-function in 1-epsilon probability and
+    performs a random action otherwise.
+    """
+
+    def __init__(self, observation_space, action_space, epsilon: float):
+        super().__init__(observation_space, action_space)
+
+        self.epsilon = epsilon
+
+    def get_action(self, observation: npt.ArrayLike) -> npt.ArrayLike:
+
+        if default_rng(42).random() < self.epsilon:
+            return self.action_space.sample()
+        else:
+            return np.random.choice(np.flatnonzero(self.value_function.values[observation] == self.value_function.values[observation].max()))
+
+    def get_action_probability(self, action: npt.ArrayLike, observation: npt.ArrayLike) -> float:
+        return 1.0 / np.count_nonzero(self.value_function.values[observation] == self.value_function.values[observation].max())
