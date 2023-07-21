d65ef1403a7088f8ff8eb8e36f42b61d0364218c
Melvin Laux <melvin.laux@dfki.de>
2023-07-21 17:53:22 +0200

    use value functions and policy implementations in sarsa

--- examples/sarsa_example.py
+++ examples/sarsa_example.py
@@ -2,10 +2,13 @@ import gymnasium as gym
 import numpy as np
 
 from modular_rl.algorithms.model_free.sarsa import Sarsa
+from modular_rl.policy.base_policy import EpsilonGreedyPolicy
 
 train_env = gym.make("FrozenLake-v1", render_mode="human", desc=["SFFH", "FFFF", "FFFF", "FFFG"])
 
-q_learning = Sarsa(train_env, 0.1, 0.1)
+
+policy = EpsilonGreedyPolicy(train_env.observation_space, train_env.action_space, epsilon=0.01)
+q_learning = Sarsa(train_env, policy, alpha=0.1)
 
 q_learning.train(100)
 
--- modular_rl/algorithms/model_free/sarsa.py
+++ modular_rl/algorithms/model_free/sarsa.py
@@ -1,5 +1,5 @@
 import numpy as np
-from modular_rl.policy.base_policy import UniformRandomPolicy, GreedyQPolicy
+from modular_rl.policy.base_policy import QValueBasedPolicy
 
 
 class Sarsa:
@@ -7,69 +7,32 @@ class Sarsa:
     Basic Sarsa using temporal differences and tabular q-values.
     """
 
-    def __init__(self, env, alpha, epsilon):
+    def __init__(self, env, policy: QValueBasedPolicy, alpha):
         self.alpha = alpha
-        self.epsilon = epsilon
         self.env = env
 
-        self.exploration_policy = UniformRandomPolicy(env.observation_space, env.action_space)
-        self.target_policy = GreedyQPolicy(env.observation_space, env.action_space, 0.0)
-
-        self.q_table = np.full(shape=(env.observation_space.n, env.action_space.n), fill_value=0.0)
+        self.policy = policy
 
     def train(self, max_episodes: int, gamma=0.99) -> None:
 
         for _ in range(max_episodes):
+
             observation, _ = self.env.reset()
+            action = self.policy.get_action(observation)
+
             while True:
-                if np.random.random_sample() < self.epsilon:
-                    action = self.exploration_policy.get_action(observation)
-                else:
-                    action = self.target_policy.get_action(observation)
 
                 next_observation, reward, terminated, truncated, info = self.env.step(action)
 
-                if np.random.random_sample() < self.epsilon:
-                    next_action = self.exploration_policy.get_action(next_observation)
-                else:
-                    next_action = self.target_policy.get_action(next_observation)
+                next_action = self.policy.get_action(next_observation)
 
-                td_error = gamma * self.q_table[next_observation, next_action] - self.q_table[observation, action]
+                td_error = reward + gamma * self.policy.value_function.get_action_value(next_observation, next_action) - \
+                           self.policy.value_function.get_action_value(observation, action)
 
-                self.q_table[observation, action] += self.alpha * (reward + td_error)
+                self.policy.value_function.update(observation, action, self.alpha * td_error)
 
                 observation = next_observation
+                action = next_action
 
                 if terminated or truncated:
-                    print(self.q_table)
                     break
-
-    def collect_episode_rollout(self):
-        """
-        Runs one full episode and returns the observations, actions and rewards.
-        """
-
-        observation = self.env.reset()[0]
-        observations = [observation]
-        actions = []
-        rewards = []
-
-        while True:
-            if np.random.random_sample() < self.epsilon:
-                action = self.exploration_policy.get_action(observation)
-            else:
-                action = self.target_policy.get_action(observation)
-
-            next_observation, reward, terminated, truncated, info = self.env.step(action)
-
-            observations.append(next_observation)
-            actions.append(action)
-            rewards.append(reward)
-
-            if terminated or truncated:
-                print("Terminated episode")
-                break
-
-        return observations, actions, rewards
-
-
