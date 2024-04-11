baa73164438da9c4490da6a3480dfa3ed7fce6eb
Alexander Fabisch <afabisch@googlemail.com>
2024-04-11 22:53:26 +0200

    Work on policy

--- modular_rl/algorithms/model_free/policy_gradient.py
+++ modular_rl/algorithms/model_free/policy_gradient.py
@@ -19,29 +19,44 @@ class EpisodeDataset:
         self.samples[-1].append((state, action, reward))
 
 
-class Policy:  # I think this must be a stochastic policy
+class SoftmaxLinearPolicy:
     state_space: gym.spaces.Space
     action_space: gym.spaces.Space
     theta = jax.Array
 
-    def __init__(self, state_space: gym.spaces.Space, action_space: gym.spaces.Space, key: jax.random.PRNGKey):
+    def __init__(
+            self,
+            state_space: gym.spaces.Space,
+            action_space: gym.spaces.Space,
+            key: jax.random.PRNGKey):
         self.state_space = state_space
         self.action_space = action_space
 
         self.theta = jax.random.normal(
-            key, (self.action_space.shape[0], self.state_space.shape[0] + 1))
+            key, (self.action_space.n + self.state_space.shape[0] + 1,))
 
-    def _project(self, state):
-        return jnp.append(state, jnp.array([1.0]))
+    def _feature_projection(self, state, action):
+        one_hot_action = jnp.zeros(self.action_space.n)
+        one_hot_action[action - self.action_space.start] = 1.0
+        return jnp.hstack((state, one_hot_action, jnp.array([1.0])))
 
-    def generate(self, state):
-        return self.theta @ self._project(state)
+    def _h(self, state, action):
+        return self.theta @ self._feature_projection(state, action)
 
-    def density(self, state, action):
-        raise NotImplementedError()
+    def action_probability(self, state, action):
+        H = jnp.exp([self._h(state, b)
+                     for b in range(self.action_space.start,
+                                    self.action_space.start + self.action_space.n)])
+        return jnp.exp(self._h(state, action)) / jnp.sum(H)  # TODO numerically robust, logsumexp?
+
+    def log_action_probability(self, state, action):
+        H = [self._h(state, b) for b in range(self.action_space.start,
+                                              self.action_space.start + self.action_space.n)]
+        h = self._h(state, action)
+        return h - jax.special.logsumexp(H)  # TODO check
 
 
-def reinforce(policy: Policy, dataset: EpisodeDataset):  # TODO can we use a pseudo-objective for autodiff?
+def reinforce(policy: SoftmaxLinearPolicy, dataset: EpisodeDataset):  # TODO can we use a pseudo-objective for autodiff?
     """REINFORCE policy gradient.
 
     References
@@ -56,6 +71,7 @@ def reinforce(policy: Policy, dataset: EpisodeDataset):  # TODO can we use a pse
         episode_return = jnp.sum(jnp.array(rewards))
         episode_returns.append(episode_return)
     episode_returns = jnp.hstack(episode_returns)
+    n_episodes = len(dataset.samples)
 
     baseline = jnp.zeros(len(policy.theta))
     for h in range(len(policy.theta)):  # can we vectorize this?
@@ -65,6 +81,10 @@ def reinforce(policy: Policy, dataset: EpisodeDataset):  # TODO can we use a pse
         # /
         # sum i=1 to len(dataset.samples)
         #     (sum t=0 to T-1 grad log pi(a_t | s_t)) ** 2
+        denoms = [jnp.sum(jnp.array([policy.log_action_probability(st, at)  # TODO gradient with respect to theta[h]
+                                     for st, at, _ in range(len(dataset.samples[i]))])) ** 2
+                  for i in range(n_episodes)]
+        noms = [denoms[i] * episode_returns[i] for i in range(n_episodes)]
         baseline[h] = 0
 
     # grad =
@@ -76,7 +96,7 @@ def reinforce(policy: Policy, dataset: EpisodeDataset):  # TODO can we use a pse
 
 if __name__ == "__main__":
     state_space = gym.spaces.Box(low=np.array([-10.0]), high=np.array([10.0]))
-    action_space = gym.spaces.Box(low=np.array([-1.0]), high=np.array([1.0]))
+    action_space = gym.spaces.Discrete(2)
     key = jax.random.key(42)
     key, subkey = jax.random.split(key)
-    policy = Policy(state_space, action_space, subkey)
+    policy = SoftmaxLinearPolicy(state_space, action_space, subkey)
