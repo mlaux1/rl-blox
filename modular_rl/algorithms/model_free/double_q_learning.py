5c98632e653f1929ed494ee1f73aa6884cd3e631
Melvin Laux <melvin.laux@gmail.com>
2024-05-13 22:06:47 +0200

    added initial implementatnion of dql

--- modular_rl/algorithms/model_free/double_q_learning.py
+++ modular_rl/algorithms/model_free/double_q_learning.py
@@ -1,5 +1,6 @@
 import gymnasium
 import jax.numpy as jnp
+import jax.random
 from jax import Array, jit, random
 from jax.random import PRNGKey
 from jax.typing import ArrayLike
@@ -25,14 +26,14 @@ def double_q_learning(
 
     for i in tqdm(range(num_episodes)):
         key, subkey = random.split(key)
-        q_table, ep_reward = _q_learning_episode(
+        q_table1, q_table2, ep_reward = _dql_episode(
             subkey, env, q_table1, q_table2, alpha, epsilon, gamma)
         ep_rewards = ep_rewards.at[i].add(ep_reward)
 
-    return q_table, ep_rewards
+    return q_table1, q_table2, ep_rewards
 
 
-def _q_learning_episode(
+def _dql_episode(
         key: PRNGKey,
         env: gymnasium.Env,
         q_table1: ArrayLike,
@@ -41,4 +42,44 @@ def _q_learning_episode(
         epsilon: float,
         gamma: float = 0.9999,
 ) -> float:
-    raise NotImplementedError
+    """Perform a single episode rollout."""
+    ep_reward = 0
+    truncated = False
+    terminated = False
+    observation, _ = env.reset()
+
+    while not terminated and not truncated:
+        key, subkey1, subkey2, subkey3 = random.split(key, 4)
+
+        # sum the q_tables and select action
+        q_table = q_table1 + q_table2
+        action = get_epsilon_greedy_action(
+            subkey1, q_table, observation, epsilon)
+        # perform environment step
+        next_observation, reward, terminated, truncated, _ = env.step(int(action))
+
+        val = jax.random.uniform(subkey3)
+        if val < 0.5:
+            q_table1 = _dql_update(
+                key, q_table1, q_table2, observation, action, reward,
+                next_observation, gamma, alpha)
+        else:
+            q_table2 = _dql_update(
+                key, q_table2, q_table1, observation, action, reward,
+                next_observation, gamma, alpha)
+
+        # housekeeping
+        observation = next_observation
+        ep_reward += reward
+
+    return q_table, ep_reward
+
+
+@jit
+def _dql_update(key, q_table1, q_table2, observation, action, reward, next_observation, gamma, alpha):
+    next_action = get_greedy_action(key, q_table1, observation)
+    val = q_table1[observation, action]
+    next_val = q_table2[next_observation, next_action]
+    error = td_error(reward, gamma, val, next_val)
+    q_table2 = q_table1.at[observation, action].add(alpha * error)
+    return q_table2
