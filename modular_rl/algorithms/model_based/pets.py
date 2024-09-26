4fc19d6a891c52d90f3c8906a6fb6bd8bc8035f7
Alexander Fabisch <alexander.fabisch@dfki.de>
2024-09-26 14:40:58 +0200

    Train ensemble fast

--- modular_rl/algorithms/model_based/pets.py
+++ modular_rl/algorithms/model_based/pets.py
@@ -1,3 +1,4 @@
+from codecs import replace_errors
 from functools import partial
 from typing import List, Tuple
 import numpy as np
@@ -10,8 +11,72 @@ from flax.training.train_state import TrainState
 import optax
 
 
+class EnsembleOfGaussianMlps:
+    """Ensemble of Gaussian MLPs.
+
+    References
+    ----------
+    .. [1] TODO PETS paper
+    """
+    def __init__(self, base_model, n_base_models, train_size, warm_start, key, verbose=0):
+        self.base_model = base_model
+        self.n_base_models = n_base_models
+        self.train_size = train_size
+        self.warm_start = warm_start
+        self.key = key
+        self.verbose = verbose
+
+    def fit(self, X, Y, n_epochs):
+        n_samples = len(X)
+        n_bootstrapped = int(self.train_size * n_samples)
+
+        if not hasattr(self, "train_states_") or not self.warm_start:
+            keys = jax.random.split(self.key, self.n_base_models + 1)
+            self.key = keys[0]
+            self.train_states_ = [
+                TrainState.create(
+                    apply_fn=self.base_model.apply,
+                    params=self.base_model.init(key, X[0]),
+                    tx=optax.adam(learning_rate=learning_rate),
+                ) for key in keys[1:]]
+
+        for i in range(self.n_base_models):
+            # TODO parallelize (pmap?)
+            # TODO mini-batches?
+            # generate individual dataset by sampling with replacement
+            self.key, bootstrapping_key = jax.random.split(self.key, 2)
+            bootstrapped_indices = jax.random.choice(
+                bootstrapping_key, n_samples, shape=(n_bootstrapped,),
+                replace=True
+            )
+            X_train = X[bootstrapped_indices]
+            Y_train = Y[bootstrapped_indices]
+
+            @jax.jit
+            def update_base_model(train_state, X, y):
+                def compute_loss(X, y, params):
+                    mean_pred, log_std_pred = self.base_model.apply(params, X)
+                    return heteroscedastic_aleatoric_uncertainty_loss(
+                        mean_pred, log_std_pred, y)
+
+                loss = partial(compute_loss, X, y)
+                loss_value, grads = jax.value_and_grad(loss)(train_state.params)
+                train_state = train_state.apply_gradients(grads=grads)
+                return loss_value, train_state
+
+            for _ in range(n_epochs):
+                loss_value, self.train_states_[i] = update_base_model(
+                    self.train_states_[i], X_train, Y_train)
+            if self.verbose:
+                print(f"base model {i + 1}, loss {loss_value:.4f}")
+
+    def predict(self, X):
+        for i in range(self.n_base_models):
+            raise NotImplementedError()
+
+
 class GaussianMlp(nn.Module):
-    """Base model of probabilistic ensemble."""
+    """Probabilistic neural network that predicts a Gaussian distribution."""
 
     shared_head: bool
     """All nodes of the last hidden layer are connected to each mean AND log_std output."""
@@ -35,34 +100,47 @@ class GaussianMlp(nn.Module):
 
 
 @jax.jit
-def heteroscedastic_aleatoric_uncertainty_loss(mean_pred, log_std_pred, y):
+def heteroscedastic_aleatoric_uncertainty_loss(mean_pred, log_std_pred, Y):
     """Heteroscedastic aleatoric uncertainty loss for Gaussian NN.
+
+    .. math::
+
+        -\log p_{\theta}(Y|X) = \frac{1}{N}\sum_n
+        \frac{1}{2} \frac{(y_n - \mu_{\theta}(x_n))^2}{\sigma_{\theta}^2(x)}
+        + \frac{1}{2} \log \sigma_{\theta}^2(x_n) + \text{constant}
+
+    This is the negative log-likelihood of Gaussian distributions predicted
+    by a neural network, i.e., the neural network predicted a mean vector and
+    a vector of component-wise log standard deviations per sample.
+
+    :param mean_pred: Means of the predicted Gaussian distributions.
+    :param log_std_pred: Logarithm of standard deviations of predicted Gaussian
+                         distributions.
+    :param Y: Actual outputs.
+    :returns: Negative log-likelihood.
 
     References
     ----------
-    .. [1] Kendall, Gal (2017). What Uncertainties Do We Need in Bayesian Deep
+    .. [1] Nix, Weigand (1994). Estimating the mean and variance of the target
+       probability distribution. in International Conference on Neural Networks
+       (ICNN). https://doi.org/10.1109/ICNN.1994.374138
+
+    .. [2] Kendall, Gal (2017). What Uncertainties Do We Need in Bayesian Deep
        Learning for Computer Vision? In Advances in Neural Information
        Processing Systems (NeurIPS). https://arxiv.org/abs/1703.04977,
        https://proceedings.neurips.cc/paper_files/paper/2017/file/2650d6089a6d640c5e85b2b88265dc2b-Paper.pdf
+
+    .. [3] Lakshminarayanan, Pritzel, Blundell (2017): Simple and Scalable
+       Predictive Uncertainty Estimation using Deep Ensembles. In Advances in
+       Neural Information Processing Systems (NeurIPS).
+       https://proceedings.neurips.cc/paper_files/paper/2017/file/9ef2ed4b7fd2c810847ffa5fa85bce38-Paper.pdf
     """
     var = jnp.exp(log_std_pred) ** 2
-    squared_erros = optax.l2_loss(mean_pred, y)  # including factor 0.5
+    # TODO what if var == 0?
+    squared_erros = optax.l2_loss(mean_pred, Y)  # including factor 0.5
     # Second term should be 0.5 * jnp.mean(jnp.log(var)), but this is the same
     # because 2 * log_std_pred == jnp.log(var), so 2 and 0.5 cancel out.
     return jnp.mean(squared_erros / var) + jnp.mean(log_std_pred)
-
-
-def update_base_model(model, train_state, X, y, n_iter):
-    @jax.jit
-    def compute_loss(X, y, params):
-        mean_pred, log_std_pred = model.apply(params, X)
-        return heteroscedastic_aleatoric_uncertainty_loss(mean_pred, log_std_pred, y)
-    loss = partial(compute_loss, X, y)
-    for _ in range(n_iter):
-        loss_value, grads = jax.value_and_grad(loss)(train_state.params)
-        train_state = train_state.apply_gradients(grads=grads)
-        print(loss_value)
-    return loss_value, train_state
 
 
 def generate_dataset1(data_key, n_samples):
@@ -101,28 +179,20 @@ learning_rate = 3e-3
 n_samples = 200
 batch_size = n_samples
 n_epochs = 5000
-net = GaussianMlp(shared_head=True, n_outputs=1, hidden_nodes=[50, 30])
 
 random_state = np.random.RandomState(seed)
 key = jax.random.PRNGKey(seed)
 
+net = GaussianMlp(shared_head=True, n_outputs=1, hidden_nodes=[50, 30])
+net.apply = jax.jit(net.apply)
+key, ensemble_key = jax.random.split(key, 2)
+ensemble = EnsembleOfGaussianMlps(net, 5, 0.5, True, ensemble_key, verbose=1)
+
 key, data_key = jax.random.split(key, 2)
 X_train, Y_train, X_test, Y_test = generate_dataset3(data_key, n_samples)
 
-key, net_key = jax.random.split(key, 2)
-train_state = TrainState.create(
-    apply_fn=net.apply,
-    params=net.init(net_key, random_state.randn(1)),
-    tx=optax.adam(learning_rate=learning_rate),
-)
-
-#indices = jnp.arange(len(X))
-#for _ in range(n_epochs):
-#    for i in range(n_samples // batch_size):
-#        batch_indices = random_state.choice(indices, batch_size, False)
-#        batch_X = X[batch_indices]
-#        batch_Y = Y[batch_indices]
-loss_value, train_state = update_base_model(net, train_state, X_train, Y_train, n_epochs)
+#loss_value, train_state = update_base_model(net, train_state, X_train, Y_train, n_epochs, verbose=1)
+ensemble.fit(X_train, Y_train, n_epochs)
 
 
 import matplotlib.pyplot as plt
@@ -131,9 +201,10 @@ import matplotlib.pyplot as plt
 plt.figure()
 plt.scatter(X_train[:, 0], Y_train[:, 0], label="Samples")
 plt.plot(X_test[:, 0], Y_test[:, 0], label="True function")
-mean, log_std = net.apply(train_state.params, X_test)
-std_196 = 1.96 * jnp.exp(log_std).squeeze()
-mean = mean.squeeze()
-plt.fill_between(X_test[:, 0], mean - std_196, mean + std_196, alpha=0.3)
-plt.plot(X_test[:, 0], mean, label="Prediction")
+for train_state in ensemble.train_states_:
+    mean, log_std = net.apply(train_state.params, X_test)
+    std_196 = 1.96 * jnp.exp(log_std).squeeze()
+    mean = mean.squeeze()
+    plt.fill_between(X_test[:, 0], mean - std_196, mean + std_196, alpha=0.3)
+    plt.plot(X_test[:, 0], mean, label="Prediction")
 plt.show()
