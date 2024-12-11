218b09f946b02157653a2f062c53c14bafff0920
Melvin Laux <laux@uni-bremen.de>
2024-12-11 18:52:47 +0100

    Add example script for DQN

---
+++ examples/dqn_example.py
@@ -0,0 +1,41 @@
+import gymnasium as gym
+import numpy as np
+
+from rl_blox.algorithms.model_free.dqn import train_dqn
+
+# Set up environment
+env_name = "Pendulum-v1"
+env = gym.make(env_name)
+seed = 1
+env = gym.wrappers.RecordEpisodeStatistics(env)
+env.action_space.seed(seed)
+envs = gym.vector.SyncVectorEnv([lambda: env])
+
+# Set up policy and q network
+policy = DeterministicMlpPolicyNetwork.create([256, 256], envs)
+q = MlpQNetwork(hidden_nodes=[256, 256])
+
+# Train
+q, q_params = train_dqn(
+    envs, q, gradient_steps=1, seed=seed, total_timesteps=31_000, verbose=1
+)
+envs.close()
+
+# Evaluate
+env = gym.make(env_name, render_mode="human")
+env = gym.wrappers.RecordEpisodeStatistics(env)
+while True:
+    done = False
+    infos = {}
+    obs, _ = env.reset()
+    while not done:
+        action = np.asarray(policy.apply(policy_params, obs)[0])
+        next_obs, reward, termination, truncation, infos = env.step(action)
+        done = termination or truncation
+        q_value = q.apply(q_params, obs, action)
+        print(f"{q_value=}")
+        obs = np.asarray(next_obs)
+    if "final_info" in infos:
+        for info in infos["final_info"]:
+            print(f"episodic_return={info['episode']['r']}")
+            break
