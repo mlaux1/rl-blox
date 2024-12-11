1d93e111ccfa8532d068bf50998831c623e3bbb4
Melvin Laux <laux@uni-bremen.de>
2024-12-11 18:53:29 +0100

    Add basic interface for DQN

--- rl_blox/algorithms/model_free/dqn.py
+++ rl_blox/algorithms/model_free/dqn.py
@@ -1,4 +1,24 @@
-def dqn(key, replay_buffer, q_network):
+import flax
+import flax.linen as nn
+
+from ..policy.replay_buffer import ReplayBuffer
+from .ddpg import MlpQNetwork
+
+
+def dqn(
+    q_network: nn.Module,
+    epsilon: float,
+    buffer_size: int = 1e6,
+    batch_size: int = 64,
+    totral_time_steps: int = 1e4,
+    gradient_steps: int = 1,
+    seed: int = 1,
+):
+    """Deep Q-Networks.
+
+    This algorithm is an off-policy value-function based RL algorithm. It uses a
+    neural network to approximate the Q-function.
+    """
     # initialise episode
     # for each step:
     # select epsilon greedy action
