27aa8851de03ce1bfd505813dd02b5027d3a4ac2
Alexander Fabisch <afabisch@googlemail.com>
2025-03-10 08:44:35 +0100

    Outline PETS control loop

--- rl_blox/algorithms/model_based/pets.py
+++ rl_blox/algorithms/model_based/pets.py
@@ -6,7 +6,33 @@ import jax.numpy as jnp
 import numpy as np
 import optax
 from flax.training.train_state import TrainState
+from collections import deque
+from numpy.typing import ArrayLike
 from rl_blox.model.gaussian_mlp_ensemble import EnsembleOfGaussianMlps
+
+
+class ReplayBuffer:
+    buffer: deque[tuple[ArrayLike, ArrayLike, float, ArrayLike, bool]]
+
+    def __init__(self, n_samples):
+        self.buffer = deque(maxlen=n_samples)
+
+    def add_samples(self, observation, action, reward, next_observation, done):
+        for i in range(len(done)):
+            self.buffer.append((observation[i], action[i], reward[i], next_observation[i], done[i]))
+
+    def sample_batch(
+            self,
+            batch_size: int,
+            rng: np.random.Generator
+    ) -> tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray]:
+        indices = rng.integers(0, len(self.buffer), batch_size)
+        observations = jnp.vstack([self.buffer[i][0] for i in indices])
+        actions = jnp.stack([self.buffer[i][1] for i in indices])
+        rewards = jnp.hstack([self.buffer[i][2] for i in indices])
+        next_observations = jnp.vstack([self.buffer[i][3] for i in indices])
+        dones = jnp.hstack([self.buffer[i][4] for i in indices])
+        return observations, actions, rewards, next_observations, dones
 
 
 def train_pets(
@@ -14,6 +40,11 @@ def train_pets(
     dynamics_model: EnsembleOfGaussianMlps,
     task_horizon: int,
     n_samples: int,
+    seed: int = 1,
+    buffer_size: int = 1_000_000,
+    total_timesteps: int = 1_000_000,
+    learning_starts: int = 25_000,
+    verbose: int = 0,
 ):
     r"""Probabilistic Ensemble - Trajectory Sampling (PE-TS).
 
@@ -57,6 +88,17 @@ def train_pets(
         Task horizon: number of time steps to predict with dynamics model.
     n_samples
         Number of action samples per time step.
+    seed, optional
+        Seed for random number generators in Jax and NumPy.
+    buffer_size, optional
+        Size of dataset for training of dynamics model.
+    total_timesteps, optional
+        Number of steps to execute in the environment.
+    learning_starts
+        Learning starts after this number of random steps was taken in the
+        environment.
+    verbose
+        Verbosity level.
 
     References
     ----------
@@ -67,4 +109,32 @@ def train_pets(
            (NeurIPS'18). Curran Associates Inc., Red Hook, NY, USA, 4759–4770.
            https://papers.nips.cc/paper_files/paper/2018/hash/3de568f8597b94bda53149c7d7f5958c-Abstract.html
     """
+    rng = np.random.default_rng(seed)
+    key = jax.random.PRNGKey(seed)
+
+    assert isinstance(env.action_space, gym.spaces.Box), "only continuous action space is supported"
+    action_space: gym.spaces.Box = env.action_space
+    rb = ReplayBuffer(buffer_size)
+
+    obs, _ = env.reset(seed=seed)
+
+    for t in range(total_timesteps):
+        if t < learning_starts:
+            action = action_space.sample()
+        else:
+            raise NotImplementedError("action generation")
+
+        next_obs, reward, termination, truncation, info = env.step(action)
+
+        if termination or truncation:
+            print(f"{t=}, length={info['episode']['l']}, "
+                  f"return={info['episode']['r']}")
+
+        rb.add_samples(obs, action, reward, next_obs, termination)
+
+        obs = next_obs
+
+        if t > learning_starts:
+            raise NotImplementedError("training procedure")
+
     raise NotImplementedError()
