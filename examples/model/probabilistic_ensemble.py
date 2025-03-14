5f1f142243dc18499de8779671a8cb307c6c7fac
Alexander Fabisch <alexander.fabisch@dfki.de>
2025-03-14 09:48:11 +0100

    Use nnx to train Gaussian MLP

---
+++ examples/model/probabilistic_ensemble.py
@@ -0,0 +1,105 @@
+import jax
+import jax.numpy as jnp
+import matplotlib.pyplot as plt
+import optax
+from flax import nnx
+
+from rl_blox.model.probabilistic_ensemble import GaussianMlp, train_step
+
+
+def generate_dataset1(data_key, n_samples):
+    x = jnp.linspace(0, 4, n_samples)
+    y_true = jnp.exp(x)
+    y = y_true + (max(x) - x) ** 2 * jax.random.normal(data_key, x.shape)
+    X_train = x[:, jnp.newaxis]
+    Y_train = y[:, jnp.newaxis]
+    return X_train, Y_train, x[:, jnp.newaxis], y_true[:, jnp.newaxis]
+
+
+def generate_dataset2(data_key, n_samples):
+    x1 = jnp.linspace(-1.0, -0.5, n_samples // 2)
+    x2 = jnp.linspace(0.5, 1.0, n_samples // 2)
+    x_train = jnp.hstack((x1, x2))
+    y_train = x_train**3 + 0.1 * jax.random.normal(data_key, x_train.shape)
+    x_test = jnp.linspace(-1.5, 1.5, n_samples)
+    y_test = x_test**3
+    return (
+        x_train[:, jnp.newaxis],
+        y_train[:, jnp.newaxis],
+        x_test[:, jnp.newaxis],
+        y_test[:, jnp.newaxis],
+    )
+
+
+def generate_dataset3(data_key, n_samples):
+    x1 = jnp.linspace(-2.0 * jnp.pi, -jnp.pi, n_samples // 2)
+    x2 = jnp.linspace(jnp.pi, 2.0 * jnp.pi, n_samples // 2)
+    x_train = jnp.hstack((x1, x2))
+    y_train = jnp.sin(x_train) + 0.1 * jax.random.normal(
+        data_key, x_train.shape
+    )
+    x_test = jnp.linspace(-3.0 * jnp.pi, 3.0 * jnp.pi, n_samples)
+    y_test = jnp.sin(x_test)
+    return (
+        x_train[:, jnp.newaxis],
+        y_train[:, jnp.newaxis],
+        x_test[:, jnp.newaxis],
+        y_test[:, jnp.newaxis],
+    )
+
+
+seed = 42
+learning_rate = 3e-3
+n_samples = 200
+batch_size = n_samples
+n_epochs = 15000
+plot_base_models = False
+
+key = jax.random.PRNGKey(seed)
+key, data_key = jax.random.split(key, 2)
+X_train, Y_train, X_test, Y_test = generate_dataset3(data_key, n_samples)
+
+model = GaussianMlp(
+    shared_head=True,
+    n_features=1,
+    n_outputs=1,
+    hidden_nodes=[100, 50],
+    rngs=nnx.Rngs(seed),
+)
+opt = nnx.Optimizer(model, optax.adam(learning_rate=learning_rate))
+
+for t in range(n_epochs):
+    loss = train_step(model, opt, X_train, Y_train)
+    print(f"{t=}: {loss=}")
+
+plt.figure()
+plt.scatter(X_train[:, 0], Y_train[:, 0], label="Samples")
+plt.plot(X_test[:, 0], Y_test[:, 0], label="True function")
+if plot_base_models:
+    for i in range(model.n_base_models):
+        mean, log_std = model.base_predict(X_test, i)
+        std_196 = 1.96 * jnp.exp(log_std).squeeze()
+        mean = mean.squeeze()
+        plt.fill_between(
+            X_test[:, 0], mean - std_196, mean + std_196, alpha=0.3
+        )
+        plt.plot(
+            X_test[:, 0], mean, ls="--", label=f"Prediction of model {i + 1}"
+        )
+mean, log_var = model(X_test)
+mean = mean.squeeze()
+std = jnp.exp(0.5 * log_var).squeeze()
+plt.plot(X_test[:, 0], mean, label="Ensemble", c="k")
+for factor in [1.0, 2.0, 3.0]:
+    plt.fill_between(
+        X_test[:, 0],
+        mean - factor * std,
+        mean + factor * std,
+        color="k",
+        alpha=0.1,
+    )
+min_y = Y_test.min()
+max_y = Y_test.max()
+plt.ylim((min_y - 2, max_y + 2))
+plt.legend(loc="best")
+plt.show()
---
+++ rl_blox/model/probabilistic_ensemble.py
@@ -0,0 +1,141 @@
+import chex
+import jax.numpy as jnp
+import optax
+from flax import nnx
+
+
+class GaussianMlp(nnx.Module):
+    """Probabilistic neural network that predicts a Gaussian distribution.
+
+    Parameters
+    ----------
+    shared_head
+        All nodes of the last hidden layer are connected to mean AND log_std.
+
+    n_inputs
+        Number of features.
+
+    n_outputs
+        Number of output components.
+
+    hidden_nodes
+        Numbers of hidden nodes of the MLP.
+
+    rngs
+        Random number generator.
+    """
+
+    def __init__(
+        self,
+        shared_head: bool,
+        n_features: int,
+        n_outputs: int,
+        hidden_nodes: list[int],
+        rngs: nnx.Rngs,
+    ):
+        chex.assert_scalar_positive(n_features)
+        chex.assert_scalar_positive(n_outputs)
+
+        self.shared_head = shared_head
+        self.n_outputs = n_outputs
+
+        self.hidden_layers = []
+        n_in = n_features
+        for n_out in hidden_nodes:
+            self.hidden_layers.append(nnx.Linear(n_in, n_out, rngs=rngs))
+            n_in = n_out
+
+        self.output_layers = []
+        if shared_head:
+            self.output_layers.append(
+                nnx.Linear(n_in, 2 * n_outputs, rngs=rngs)
+            )
+        else:
+            self.output_layers.append(nnx.Linear(n_in, n_outputs, rngs=rngs))
+            self.output_layers.append(nnx.Linear(n_in, n_outputs, rngs=rngs))
+
+    def __call__(self, x: jnp.ndarray) -> tuple[jnp.ndarray, jnp.ndarray]:
+        for layer in self.hidden_layers:
+            x = nnx.swish(layer(x))
+
+        if self.shared_head:
+            y = self.output_layers[0](x)
+            mean, log_var = jnp.split(y, (self.n_outputs,), axis=-1)
+        else:
+            mean = self.output_layers[0](x)
+            log_var = self.output_layers[1](x)
+
+        return mean, log_var
+
+
+def gaussian_nll(
+    mean_pred: jnp.ndarray, log_var_pred: jnp.ndarray, Y: jnp.ndarray
+) -> jnp.ndarray:
+    r"""Heteroscedastic aleatoric uncertainty loss for Gaussian NN.
+
+    This is the negative log-likelihood of Gaussian distributions
+    :math:`p_{\theta}(Y|X)` predicted by a neural network, i.e., the neural
+    network predicted a mean vector :math:`\mu_{\theta}(x_n)` and a vector of
+    component-wise log variances :math:`\sigma_{\theta}^2(x_n)` per sample:
+
+    .. math::
+
+        -\log p_{\theta}(Y|X) = \frac{1}{N}\sum_n
+        \frac{1}{2} \frac{(y_n - \mu_{\theta}(x_n))^2}{\sigma_{\theta}^2(x)}
+        + \frac{1}{2} \log \sigma_{\theta}^2(x_n) + \text{constant}
+
+    The loss was originally proposed by Nix and Weigand [1]_.
+
+    Parameters
+    ----------
+    mean_pred : array, shape (n_samples, n_outputs)
+        Means of the predicted Gaussian distributions.
+
+    log_var_pred : array, shape (n_samples, n_outputs)
+        Logarithm of variances of predicted Gaussian distributions.
+
+    Y : array, shape (n_samples, n_outputs)
+        Actual outputs.
+
+    Returns
+    -------
+    nll
+        Negative log-likelihood.
+
+    References
+    ----------
+    .. [1] Nix, Weigand (1994). Estimating the mean and variance of the target
+       probability distribution. in International Conference on Neural Networks
+       (ICNN). https://doi.org/10.1109/ICNN.1994.374138
+
+    .. [2] Kendall, Gal (2017). What Uncertainties Do We Need in Bayesian Deep
+       Learning for Computer Vision? In Advances in Neural Information
+       Processing Systems (NeurIPS). https://arxiv.org/abs/1703.04977,
+       https://proceedings.neurips.cc/paper_files/paper/2017/file/2650d6089a6d640c5e85b2b88265dc2b-Paper.pdf
+
+    .. [3] Lakshminarayanan, Pritzel, Blundell (2017): Simple and Scalable
+       Predictive Uncertainty Estimation using Deep Ensembles. In Advances in
+       Neural Information Processing Systems (NeurIPS).
+       https://proceedings.neurips.cc/paper_files/paper/2017/file/9ef2ed4b7fd2c810847ffa5fa85bce38-Paper.pdf
+    """
+    chex.assert_equal_shape((mean_pred, Y))
+    chex.assert_equal_shape((log_var_pred, Y))
+
+    inv_var = jnp.exp(-log_var_pred)  # exp(-log_var) == 1.0 / exp(log_var)
+    squared_errors = optax.l2_loss(mean_pred, Y)  # including factor 0.5
+    return jnp.mean(squared_errors * inv_var) + 0.5 * jnp.mean(log_var_pred)
+
+
+@nnx.jit
+def train_step(model, optimizer, X, Y):
+    def loss(model):
+        mean, log_var = model(X)
+        # TODO replace by numerically safe method
+        # logvar = max_logvar - jax.nn.softplus(max_logvar - logvar)
+        # logvar = min_logvar + jax.nn.softplus(logvar - min_logvar)
+        return gaussian_nll(mean, log_var, Y)
+
+    loss, grads = nnx.value_and_grad(loss)(model)
+    optimizer.update(grads)
+
+    return loss
