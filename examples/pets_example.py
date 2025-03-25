0e8a9b53d40dc395db7091eb99046e167f53c5e5
Alexander Fabisch <alexander.fabisch@dfki.de>
2025-03-25 16:56:58 +0100

    More regularization

--- examples/pets_example.py
+++ examples/pets_example.py
@@ -26,7 +26,7 @@ dynamics_model = EnsembleTrainState(
     optimizer=nnx.Optimizer(model, optax.adam(learning_rate=1e-3)),
     train_size=0.7,
     batch_size=32,
-    regularization=0.0005,
+    regularization=10.0,
 )
 mpc = train_pets(
     env,
