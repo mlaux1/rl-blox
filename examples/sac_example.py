Fix scaling

examples/sac_example.py
@@ -6,6 +6,7 @@ from rl_blox.algorithm.sac import (
     create_sac_state,
     train_sac,
     NormalizeObservationStreamX,
+    ScaleRewardStreamX,
 )
 from rl_blox.logging.logger import AIMLogger
 
@@ -13,6 +14,7 @@ env_name = "Pendulum-v1"
 env = gym.make(env_name)
 seed = 1
 verbose = 1
+gamma = 0.99
 env = gym.wrappers.RecordEpisodeStatistics(env)
 
 hparams_models = dict(
@@ -25,7 +27,7 @@ hparams_models = dict(
 hparams_algorithm = dict(
     total_timesteps=11_000,
     buffer_size=11_000,
-    gamma=0.99,
+    gamma=gamma,
     learning_starts=5_000,
 )
 
@@ -51,11 +53,12 @@ sac_result = train_sac(
     sac_state.q2,
     sac_state.q2_optimizer,
     observation_normalizer=NormalizeObservationStreamX(),
+    reward_scaler=ScaleRewardStreamX(gamma),
     logger=logger,
     **hparams_algorithm,
 )
 env.close()
-policy, _, q1, _, _, q2, _, _, _ = sac_result
+policy, _, q1, _, _, q2, _, _, _, obs_norm, _ = sac_result
 
 
 # Evaluation
@@ -65,17 +68,17 @@ while True:
     done = False
     obs, _ = env.reset()
     while not done:
-        action = np.asarray(policy(jnp.asarray(obs))[0])
+        obs_in = jnp.asarray(obs)
+        if obs_norm is not None:
+            obs_in = obs_norm.transform(obs_in)
+        action = np.asarray(policy(obs_in))
         next_obs, reward, termination, truncation, info = env.step(action)
         done = termination or truncation
         obs = np.asarray(next_obs)
 
         if verbose >= 2:
-            q1_value = float(
-                q1(jnp.concatenate((obs, action), axis=-1)).squeeze()
-            )
-            q2_value = float(
-                q2(jnp.concatenate((obs, action), axis=-1)).squeeze()
-            )
+            obs_act = jnp.concatenate((obs_in, action), axis=-1)
+            q1_value = float(q1(obs_act).squeeze())
+            q2_value = float(q2(obs_act).squeeze())
             q_value = min(q1_value, q2_value)
             print(f"{q_value=:.3f} {q1_value=:.3f} {q2_value=:.3f}")

rl_blox/algorithm/sac.py
@@ -528,6 +528,8 @@ def train_sac(
     nnx.Module,
     nnx.Optimizer,
     EntropyControl,
+    NormalizeObservationBase,
+    ScaleRewardBase,
 ]:
     r"""Soft actor-critic (SAC).
 
@@ -620,6 +622,10 @@ def train_sac(
         Optimizer of q2.
     entropy_control
         State of entropy tuning.
+    observation_normalizer
+        State of observation normalizer.
+    reward_scaler
+        State of reward scaler.
 
     References
     ----------
@@ -678,8 +684,11 @@ def train_sac(
             action = env.action_space.sample()
         else:
             key, action_key = jax.random.split(key, 2)
+            obs_in = jnp.asarray(obs)
+            if observation_normalizer is not None:
+                obs_in = observation_normalizer.transform(obs_in)
             action = np.asarray(
-                _sample_action(policy, jnp.asarray(obs), action_key)
+                _sample_action(policy, jnp.asarray(obs_in), action_key)
             )
 
         next_obs, reward, termination, truncation, info = env.step(action)
@@ -800,6 +809,8 @@ def train_sac(
         q2_target,
         q2_optimizer,
         entropy_control,
+        observation_normalizer,
+        reward_scaler,
     )
 
 