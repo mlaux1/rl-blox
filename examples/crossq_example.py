bce00963af0c64f064ae6c5a184037af028f6b52
Alexander Fabisch <afabisch@googlemail.com>
2025-04-15 10:29:42 +0200

    Use final policy

--- examples/crossq_example.py
+++ examples/crossq_example.py
@@ -1,3 +1,4 @@
+import numpy as np
 import gymnasium as gym
 from rl_blox.algorithms.model_free.ext.crossq import train_crossq
 
@@ -7,4 +8,26 @@ env = gym.make(env_name)
 seed = 1
 env = gym.wrappers.RecordEpisodeStatistics(env)
 env.action_space.seed(seed)
-train_crossq(env)
+model = train_crossq(
+    env,
+    total_timesteps=15_000,
+    seed=seed,
+)
+env.close()
+
+# Evaluation
+env = gym.make(env_name, render_mode="human")
+env = gym.wrappers.RecordEpisodeStatistics(env)
+while True:
+    done = False
+    infos = {}
+    obs, _ = env.reset()
+    while not done:
+        action, _ = model.predict(obs, deterministic=True)
+        next_obs, reward, termination, truncation, infos = env.step(action)
+        done = termination or truncation
+        obs = next_obs
+    if "final_info" in infos:
+        for info in infos["final_info"]:
+            print(f"episodic_return={info['episode']['r']}")
+            break
