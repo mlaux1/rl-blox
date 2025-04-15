a6068381ec768e6e670c7d8e4445825d3b45f029
Alexander Fabisch <afabisch@googlemail.com>
2025-04-15 14:50:08 +0200

    Fix config

--- rl_blox/algorithms/model_based/ext/tdmpc2.py
+++ rl_blox/algorithms/model_based/ext/tdmpc2.py
@@ -50,7 +50,6 @@ import gymnasium as gym
 
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.nn import Buffer
 
 from tensordict.nn import TensorDictParams
 from tensordict import from_modules
@@ -242,8 +241,8 @@ class RunningScale(torch.nn.Module):
 	def __init__(self, cfg):
 		super().__init__()
 		self.cfg = cfg
-		self.value = Buffer(torch.ones(1, dtype=torch.float32, device=torch.device('cuda:0')))
-		self._percentiles = Buffer(torch.tensor([5, 95], dtype=torch.float32, device=torch.device('cuda:0')))
+		self.value = torch.nn.Buffer(torch.ones(1, dtype=torch.float32, device=torch.device('cuda:0')))
+		self._percentiles = torch.nn.Buffer(torch.tensor([5, 95], dtype=torch.float32, device=torch.device('cuda:0')))
 
 	def state_dict(self):
 		return dict(value=self.value, percentiles=self._percentiles)
@@ -350,13 +349,7 @@ class Logger:
 		if not cfg.enable_wandb or self.project == "none" or self.entity == "none":
 			print(colored("Wandb disabled.", "blue", attrs=["bold"]))
 			cfg.save_agent = False
-			cfg.save_video = False
-			self._video = None
 		self._wandb = None
-
-	@property
-	def video(self):
-		return self._video
 
 	@property
 	def model_dir(self):
@@ -471,28 +464,27 @@ class TensorWrapper(gym.Wrapper):
 
 def make_env(cfg):
 	"""Make an environment for TD-MPC2 experiments."""
-	gym.logger.set_level(40)
+	gym.logger.min_level = 40
 	if cfg.multitask:
 		raise NotImplementedError("deleted from original source")
 	else:
-		env = gym.make(cfg["task"])  # TODO fix cfg
+		env = gym.make(cfg.task)
 		env = TensorWrapper(env)
 	try: # Dict
 		cfg.obs_shape = {k: v.shape for k, v in env.observation_space.spaces.items()}
 	except: # Box
 		cfg.obs_shape = {cfg.get('obs', 'state'): env.observation_space.shape}
 	cfg.action_dim = env.action_space.shape[0]
-	cfg.episode_length = env.max_episode_steps
+	cfg.episode_length = env.spec.max_episode_steps
 	cfg.seed_steps = max(1000, 5*cfg.episode_length)
 	return env
 
 
-def cfg_to_dataclass(cfg, frozen=False):
+def cfg_to_dataclass(cfg_dict, frozen=False):
 	"""
 	Converts an OmegaConf config to a dataclass object.
 	This prevents graph breaks when used with torch.compile.
 	"""
-	cfg_dict = OmegaConf.to_container(cfg)
 	fields = []
 	for key, value in cfg_dict.items():
 		fields.append((key, Any, dataclasses.field(default_factory=lambda value_=value: value_)))
@@ -508,17 +500,8 @@ def parse_cfg(cfg: OmegaConf) -> OmegaConf:
 	"""
 	Parses a Hydra config. Mostly for convenience.
 	"""
-
-	# Logic
-	for k in cfg.keys():
-		try:
-			v = cfg[k]
-			if v == None:
-				v = True
-		except:
-			pass
-
 	# Algebraic expressions
+	cfg = cfg._asdict()
 	for k in cfg.keys():
 		try:
 			v = cfg[k]
@@ -532,28 +515,28 @@ def parse_cfg(cfg: OmegaConf) -> OmegaConf:
 			pass
 
 	# Convenience
-	cfg.work_dir = Path(hydra.utils.get_original_cwd()) / 'logs' / cfg.task / str(cfg.seed) / cfg.exp_name
-	cfg.task_title = cfg.task.replace("-", " ").title()
-	cfg.bin_size = (cfg.vmax - cfg.vmin) / (cfg.num_bins-1) # Bin size for discrete regression
+	cfg["work_dir"] = Path(".") / 'logs' / cfg["task"] / str(cfg["seed"]) / cfg["exp_name"]
+	cfg["task_title"] = cfg["task"].replace("-", " ").title()
+	cfg["bin_size"] = (cfg["vmax"] - cfg["vmin"]) / (cfg["num_bins"]-1) # Bin size for discrete regression
 
 	# Model size
-	if cfg.get('model_size', None) is not None:
-		assert cfg.model_size in MODEL_SIZE.keys(), \
-			f'Invalid model size {cfg.model_size}. Must be one of {list(MODEL_SIZE.keys())}'
-		for k, v in MODEL_SIZE[cfg.model_size].items():
+	if cfg.get("model_size", None) is not None:
+		assert cfg["model_size"] in MODEL_SIZE.keys(), \
+			f"Invalid model size {cfg['model_size']}. Must be one of {list(MODEL_SIZE.keys())}"
+		for k, v in MODEL_SIZE[cfg["model_size"]].items():
 			cfg[k] = v
-		if cfg.task == 'mt30' and cfg.model_size == 19:
-			cfg.latent_dim = 512 # This checkpoint is slightly smaller
+		if cfg["task"] == "mt30" and cfg["model_size"] == 19:
+			cfg["latent_dim"] = 512 # This checkpoint is slightly smaller
 
 	# Multi-task
-	cfg.multitask = cfg.task in TASK_SET.keys()
-	if cfg.multitask:
-		cfg.task_title = cfg.task.upper()
+	cfg["multitask"] = cfg["task"] in TASK_SET.keys()
+	if cfg["multitask"]:
+		cfg["task_title"] = cfg.task.upper()
 		# Account for slight inconsistency in task_dim for the mt30 experiments
-		cfg.task_dim = 96 if cfg.task == 'mt80' or cfg.get('model_size', 5) in {1, 317} else 64
+		cfg["task_dim"] = 96 if cfg.task == "mt80" or cfg.get("model_size", 5) in {1, 317} else 64
 	else:
-		cfg.task_dim = 0
-	cfg.tasks = TASK_SET.get(cfg.task, [cfg.task])
+		cfg["task_dim"] = 0
+	cfg["tasks"] = TASK_SET.get(cfg["task"], [cfg["task"]])
 
 	return cfg_to_dataclass(cfg)
 
@@ -699,14 +682,14 @@ class OnlineTrainer(Trainer):
 		super().__init__(*args, **kwargs)
 		self._step = 0
 		self._ep_idx = 0
-		self._start_time = time()
+		self._start_time = time.time()
 
 	def common_metrics(self):
 		"""Return a dictionary of current metrics."""
 		return dict(
 			step=self._step,
 			episode=self._ep_idx,
-			total_time=time() - self._start_time,
+			total_time=time.time() - self._start_time,
 		)
 
 	def eval(self):
@@ -714,20 +697,14 @@ class OnlineTrainer(Trainer):
 		ep_rewards, ep_successes = [], []
 		for i in range(self.cfg.eval_episodes):
 			obs, done, ep_reward, t = self.env.reset(), False, 0, 0
-			if self.cfg.save_video:
-				self.logger.video.init(self.env, enabled=(i==0))
 			while not done:
 				torch.compiler.cudagraph_mark_step_begin()
 				action = self.agent.act(obs, t0=t==0, eval_mode=True)
 				obs, reward, done, info = self.env.step(action)
 				ep_reward += reward
 				t += 1
-				if self.cfg.save_video:
-					self.logger.video.record(self.env)
 			ep_rewards.append(ep_reward)
 			ep_successes.append(info['success'])
-			if self.cfg.save_video:
-				self.logger.video.save(self._step)
 		return dict(
 			episode_reward=np.nanmean(ep_rewards),
 			episode_success=np.nanmean(ep_successes),
@@ -1555,7 +1532,7 @@ def zero_(params):
 		p.data.fill_(0)
 
 
-def train_tdmpc2(**cfg):
+def train_tdmpc2(cfg):
 	"""TD-MPC2.
 
 	Most relevant args:
