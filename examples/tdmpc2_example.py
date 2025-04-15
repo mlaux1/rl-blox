88474935796756ce953d49b4d178680b16ace8ed
Alexander Fabisch <afabisch@googlemail.com>
2025-04-15 14:30:23 +0200

    Add original code of TD-MPC2

---
+++ examples/tdmpc2_example.py
@@ -0,0 +1,90 @@
+import gymnasium as gym
+import numpy as np
+
+from rl_blox.algorithms.model_based.ext.tdmpc2 import train_tdmpc2
+
+env_name = "Pendulum-v1"
+env = gym.make(env_name)
+seed = 1
+env = gym.wrappers.RecordEpisodeStatistics(env)
+env.action_space.seed(seed)
+
+train_tdmpc2(
+    task="dog-run",
+    obs="state",
+    checkpoint="???",
+    # eval
+    eval_episodes=10,
+    eval_freq=50000,
+    steps=10_000_000,
+    # training
+    batch_size=256,
+    reward_coef=0.1,
+    value_coef=0.1,
+    consistency_coef=20,
+    rho=0.5,
+    lr=3e-4,
+    enc_lr_scale=0.3,
+    grad_clip_norm=20,
+    tau=0.01,
+    discount_denom=5,
+    discount_min=0.95,
+    discount_max=0.995,
+    buffer_size=1_000_000,
+    exp_name="default",
+    data_dir="???",
+    # planning
+    mpc=True,
+    iterations=6,
+    num_samples=512,
+    num_elites=64,
+    num_pi_trajs=24,
+    horizon=3,
+    min_std=0.05,
+    max_std=2,
+    temperature=0.5,
+    # actor
+    log_std_min=-10,
+    log_std_max=2,
+    entropy_coef=1e-4,
+    # critic
+    num_bins=101,
+    vmin=-10,
+    vmax=+10,
+    # architecture
+    model_size="???",
+    num_enc_layers=2,
+    enc_dim=256,
+    num_channels=32,
+    mlp_dim=512,
+    latent_dim=512,
+    task_dim=96,
+    num_q=5,
+    dropout=0.01,
+    simnorm_dim=8,
+    # logging
+    wandb_project="???",
+    wandb_entity="???",
+    wandb_silent=False,
+    enable_wandb=True,
+    save_csv=True,
+    # misc
+    save_video=True,
+    save_agent=True,
+    seed=1,
+    # convenience
+    work_dir="???",
+    task_title="???",
+    multitask="???",
+    tasks="???",
+    obs_shape="???",
+    action_dim="???",
+    episode_length="???",
+    obs_shapes="???",
+    action_dims="???",
+    episode_lengths="???",
+    seed_steps="???",
+    bin_size="???",
+    # speedups
+    compile=False,
+)
