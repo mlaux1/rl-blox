3f5876c01053f9f46738c812f1336cf64a2d3580
Melvin Laux <melvin.laux@dfki.de>
2025-04-30 17:05:14 +0200

    Merge tools and helper into util module

--- rl_blox/util/gymtools.py
+++ rl_blox/util/gymtools.py
@@ -4,9 +4,8 @@ from gymnasium.spaces.utils import flatdim
 
 def space_shape(space):
     """Get shape for value function table from space."""
-    if (  # this is only one simple hack so far, but... YAGNI!
-        isinstance(space, gym.spaces.Tuple)
-        and isinstance(space[0], gym.spaces.Discrete)
-    ):
+    if isinstance(  # this is only one simple hack so far, but... YAGNI!
+        space, gym.spaces.Tuple
+    ) and isinstance(space[0], gym.spaces.Discrete):
         return tuple([space.n for space in space])
     return (flatdim(space),)
