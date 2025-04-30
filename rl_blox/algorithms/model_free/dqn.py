75291a763b6ce80b0270c680481b1416de3c287f
Melvin Laux <melvin.laux@dfki.de>
2025-04-30 16:17:21 +0200

    Refactor formatting

--- rl_blox/algorithms/model_free/dqn.py
+++ rl_blox/algorithms/model_free/dqn.py
@@ -216,7 +216,9 @@ def train_dqn(
     References
     ----------
 
-    .. [1] Mnih, V., Kavukcuoglu, K., Silver, D., Graves, A., Antonoglou, I., Wierstra, D., & Riedmiller, M. (2013). Playing atari with deep reinforcement learning. arXiv preprint arXiv:1312.5602.
+    .. [1] Mnih, V., Kavukcuoglu, K., Silver, D., Graves, A., Antonoglou, I.,
+       Wierstra, D., & Riedmiller, M. (2013). Playing atari with deep
+       reinforcement learning. arXiv preprint arXiv:1312.5602.
 
     """
 
