f0acc032da3bef850e1863fa3ba344723d3eaea0
Alexander Fabisch <alexander.fabisch@dfki.de>
2025-05-02 09:46:00 +0200

    Integrate AIM logger

--- examples/crossq_example.py
+++ examples/crossq_example.py
@@ -1,17 +1,33 @@
 import gymnasium as gym
+
 from rl_blox.algorithm.ext.crossq import train_crossq
+from rl_blox.logging.logger import AIMLogger
 
 
 env_name = "Pendulum-v1"
 env = gym.make(env_name)
 seed = 1
+verbose = 1
 env = gym.wrappers.RecordEpisodeStatistics(env)
 env.action_space.seed(seed)
-model = train_crossq(
-    env,
+
+if verbose:
+    print(
+        "This example uses the AIM logger. You will not see any output on "
+        "stdout. Run 'aim up' to analyze the progress."
+    )
+hparams = dict(
     total_timesteps=15_000,
     seed=seed,
 )
+logger = AIMLogger()
+logger.define_experiment(
+    env_name=env_name,
+    algorithm_name="CrossQ",
+    hparams=hparams,
+)
+
+model = train_crossq(env, logger=logger, **hparams)
 env.close()
 
 # Evaluation
