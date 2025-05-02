314daf692a823979e3e142d4560e0b762ac1064e
Melvin Laux <laux@uni-bremen.de>
2025-05-02 15:57:17 +0200

    Add monte carlo basic test

--- tests/test_monte_carlo.py
+++ tests/test_monte_carlo.py
@@ -1,4 +1,10 @@
-class TestMonteCarlo:
-    def test_monte_carlo(self):
-        # TODO
-        pass
+import gymnasium as gym
+
+from rl_blox.algorithm.monte_carlo import MonteCarlo
+
+
+def test_monte_carlo():
+    env = gym.make("FrozenLake-v1")
+    mc = MonteCarlo(env, 0.1, "first_visit")
+    mc.train(1)
+    env.close()
