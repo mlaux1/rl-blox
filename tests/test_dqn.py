761161758fbeda8ecc1068d33e02683727cc9d40
Melvin Laux <laux@uni-bremen.de>
2025-05-02 21:00:21 +0200

    Use new unified MLP

--- tests/test_dqn.py
+++ tests/test_dqn.py
@@ -2,18 +2,24 @@ import gymnasium as gym
 import optax
 from flax import nnx
 
-from rl_blox.algorithm.dqn import MLP, train_dqn
+from rl_blox.algorithm.dqn import train_dqn
+from rl_blox.blox.function_approximator.mlp import MLP
 from rl_blox.blox.replay_buffer import ReplayBuffer
 
 
 def test_q_learning():
     env = gym.make("CartPole-v1")
     seed = 42
-    nnx_rngs = nnx.Rngs(seed)
 
     rb = ReplayBuffer(100)
 
-    q_net = MLP(4, 10, env.action_space.n, nnx_rngs)
+    q_net = MLP(
+        env.observation_space.shape[0],
+        int(env.action_space.n),
+        [10],
+        "relu",
+        nnx.Rngs(seed),
+    )
 
     optimizer = nnx.Optimizer(q_net, optax.rprop(0.0003))
 
