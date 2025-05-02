de0fbc7e7b0b043ee8f55a6c8da5b35ebed04aba
Melvin Laux <laux@uni-bremen.de>
2025-05-02 15:26:44 +0200

    Add q-learning basic test

---
+++ tests/test_q_learning.py
@@ -0,0 +1,21 @@
+import gymnasium as gym
+import jax
+
+from rl_blox.algorithm.q_learning import q_learning
+from rl_blox.blox.value_policy import make_q_table
+
+
+def test_q_learning():
+    env = gym.make("CliffWalking-v0")
+    q_table = make_q_table(env)
+
+    q_table, ep_rewards = q_learning(
+        jax.random.key(0),
+        env,
+        q_table,
+        alpha=0.05,
+        epsilon=0.05,
+        num_episodes=1,
+    )
+
+    env.close()
