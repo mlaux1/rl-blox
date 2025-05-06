6c245407b8f947cc16cc17232d62176607d448b7
Melvin Laux <melvin.laux@dfki.de>
2025-05-06 17:44:44 +0200

    Update sarsa test to new interface

--- tests/test_sarsa.py
+++ tests/test_sarsa.py
@@ -1,7 +1,7 @@
 import gymnasium as gym
 import jax
 
-from rl_blox.algorithm.sarsa import sarsa
+from rl_blox.algorithm.sarsa import train_sarsa
 from rl_blox.blox.value_policy import make_q_table
 
 
@@ -9,13 +9,13 @@ def test_sarsa():
     env = gym.make("CliffWalking-v0")
     q_table = make_q_table(env)
 
-    _ = sarsa(
+    _ = train_sarsa(
         jax.random.key(0),
         env,
         q_table,
         alpha=0.05,
         epsilon=0.05,
-        num_episodes=1,
+        total_timesteps=1,
     )
 
     env.close()
