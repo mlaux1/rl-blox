6f8b3884f6dfdb93670652336cee7d6d32c6930a
Melvin Laux <melvin.laux@dfki.de>
2025-05-06 17:40:01 +0200

    Update sarsa interface

--- rl_blox/algorithm/sarsa.py
+++ rl_blox/algorithm/sarsa.py
@@ -1,6 +1,6 @@
 import gymnasium
 import jax.numpy as jnp
-from jax import Array, jit, random
+from jax import jit, random
 from jax.random import PRNGKey
 from jax.typing import ArrayLike
 from tqdm import tqdm
@@ -9,25 +9,67 @@ from ..blox.value_policy import get_epsilon_greedy_action
 from ..util.error_functions import td_error
 
 
-def sarsa(
+def train_sarsa(
     key: PRNGKey,
     env: gymnasium.Env,
     q_table: ArrayLike,
     alpha: float,
     epsilon: float,
-    num_episodes: int,
     gamma: float = 0.9999,
-) -> Array:
-    ep_rewards = jnp.zeros(num_episodes)
+    total_timesteps: int = 10_000,
+) -> ArrayLike:
+    r"""
+    State-action-reward-state-action algorithm.
 
-    for i in tqdm(range(num_episodes)):
+    This function implements the SARSA (State-Action-Reward-State-Action) update rule
+    in the context of tabular reinforcement learning using JAX. The update is
+    on-policy and uses the next action chosen by the current policy.
+
+    Parameters
+    ----------
+    key : PRNGKey
+        The random key.
+    env : gymnasium.Env
+        The environment to train on.
+    q_table : ArrayLike
+        The Q-table of shape (num_states, num_actions), containing current Q-values.
+    alpha : float
+        The learning rate, determining how much new information overrides old.
+    epsilon : float
+        The tradeoff for random exploration.
+    gamma : float
+        The discount factor, representing the importance of future rewards.
+    total_timesteps : int
+        The number of total timesteps to train for.
+
+    Returns
+    -------
+    q_table : jax.numpy.ndarray
+        The updated Q-table after applying the SARSA update rule.
+
+    References
+    ----------
+    1. Rummery, G. A., & Niranjan, M. (1994). *On-line Q-learning using connectionist systems*.
+       Technical Report CUED/F-INFENG/TR 166, Department of Engineering, University of Cambridge.
+       URL: [https://mi.eng.cam.ac.uk/reports/svr-ftp/auto-pdf/rummery_tr166.pdf](https://mi.eng.cam.ac.uk/reports/svr-ftp/auto-pdf/rummery_tr166.pdf)
+
+    2. Sutton, R. S., & Barto, A. G. (1998). *Reinforcement Learning: An Introduction* (2nd ed.).
+       MIT Press. ISBN 978-0262039246.
+
+    3. Singh, S. P., & Sutton, R. S. (1996). *Reinforcement learning with replacing eligibility traces*.
+       Machine Learning, 22(1-3), 123–158.
+       DOI: [10.1007/BF00114726](https://link.springer.com/article/10.1007/BF00114726)
+    """
+    ep_rewards = jnp.zeros(total_timesteps)
+
+    for i in tqdm(range(total_timesteps)):
         key, subkey = random.split(key)
         q_table, ep_reward = _sarsa_episode(
             subkey, env, q_table, alpha, epsilon, gamma
         )
         ep_rewards = ep_rewards.at[i].add(ep_reward)
 
-    return q_table, ep_rewards
+    return q_table
 
 
 def _sarsa_episode(
