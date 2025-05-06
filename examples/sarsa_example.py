de78185ae17a0aaccf94923a1b45eb6b2073189d
Melvin Laux <melvin.laux@dfki.de>
2025-05-06 18:54:20 +0200

    Fix bug where incorrect q_table was used for final policy

--- examples/sarsa_example.py
+++ examples/sarsa_example.py
@@ -5,12 +5,12 @@ from gymnasium.wrappers import RecordEpisodeStatistics
 from jax.random import PRNGKey
 
 from rl_blox.algorithm.sarsa import train_sarsa
-from rl_blox.blox.value_policy import get_greedy_action, make_q_table
+from rl_blox.blox.value_policy import get_epsilon_greedy_action, make_q_table
 from rl_blox.util.experiment_helper import generate_rollout
 
-NUM_TIMESTEPS = 200_000
-LEARNING_RATE = 0.05
-EPSILON = 0.05
+NUM_TIMESTEPS = 200_00
+LEARNING_RATE = 0.1
+EPSILON = 0.2
 KEY = PRNGKey(42)
 WINDOW_SIZE = 10
 ENV_NAME = "CliffWalking-v0"
@@ -20,7 +20,7 @@ env = RecordEpisodeStatistics(env, buffer_length=2000)
 
 q_table = make_q_table(env)
 
-ep_rewards = train_sarsa(
+q_table = train_sarsa(
     KEY,
     env,
     q_table,
@@ -32,7 +32,11 @@ ep_rewards = train_sarsa(
 env.close()
 
 # create and run the final policy
-policy = partial(get_greedy_action, key=KEY, q_table=q_table)
+policy = partial(
+    get_epsilon_greedy_action, key=KEY, q_table=q_table, epsilon=0.2
+)
+
+print(q_table)
 
 test_env = gym.make(ENV_NAME, render_mode="human")
 generate_rollout(test_env, policy)
