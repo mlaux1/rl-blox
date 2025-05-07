8fe4b186a46644b86a8856cdfe71303c3f5fb352
Melvin Laux <laux@uni-bremen.de>
2025-05-07 17:55:40 +0200

    Update DQL example

--- examples/double_q_learning_example.py
+++ examples/double_q_learning_example.py
@@ -2,40 +2,38 @@ from functools import partial
 
 import gymnasium as gym
 from gymnasium.wrappers import RecordEpisodeStatistics
-from jax.random import PRNGKey
 
-from rl_blox.algorithm.double_q_learning import double_q_learning
+from rl_blox.algorithm.double_q_learning import train_double_q_learning
 from rl_blox.blox.value_policy import get_greedy_action, make_q_table
 from rl_blox.util.experiment_helper import generate_rollout
 
-NUM_EPISODES = 3000
-LEARNING_RATE = 0.05
-EPSILON = 0.05
-KEY = PRNGKey(42)
-WINDOW_SIZE = 10
+NUM_STEPS = 50_000
+LEARNING_RATE = 0.1
+EPSILON = 0.1
 ENV_NAME = "CliffWalking-v0"
 
 env = gym.make(ENV_NAME)
-env = RecordEpisodeStatistics(env, buffer_length=NUM_EPISODES)
+env = RecordEpisodeStatistics(env, buffer_length=2000)
 
 q_table1 = make_q_table(env)
 q_table2 = make_q_table(env)
 
-q_table1, q_table2, ep_rewards = double_q_learning(
-    KEY,
+q_table1, q_table2 = train_double_q_learning(
     env,
     q_table1,
     q_table2,
-    alpha=LEARNING_RATE,
+    learning_rate=LEARNING_RATE,
     epsilon=EPSILON,
-    num_episodes=NUM_EPISODES,
+    total_timesteps=NUM_STEPS,
+    seed=42,
 )
 
 env.close()
 
+
 # create and run the final policy
 q_table = q_table1 + q_table2
-policy = partial(get_greedy_action, key=KEY, q_table=q_table)
+policy = partial(get_greedy_action, key=42, q_table=q_table)
 
 test_env = gym.make(ENV_NAME, render_mode="human")
 generate_rollout(test_env, policy)
