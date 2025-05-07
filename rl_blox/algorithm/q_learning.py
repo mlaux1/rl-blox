fe805ca13d78030e8e0ee239170649c032971a37
Melvin Laux <laux@uni-bremen.de>
2025-05-07 17:06:43 +0200

    Update q-learning interface

--- rl_blox/algorithm/q_learning.py
+++ rl_blox/algorithm/q_learning.py
@@ -1,69 +1,46 @@
 import gymnasium
-import jax.numpy as jnp
-from jax import Array, jit, random
-from jax.random import PRNGKey
+import jax
+import tqdm
 from jax.typing import ArrayLike
-from tqdm import tqdm
 
 from ..blox.value_policy import get_epsilon_greedy_action, get_greedy_action
+from ..logging.logger import LoggerBase
 from ..util.error_functions import td_error
 
 
-def q_learning(
-    key: PRNGKey,
+def train_q_learning(
     env: gymnasium.Env,
     q_table: ArrayLike,
-    alpha: float,
-    epsilon: float,
-    num_episodes: int,
-    gamma: float = 0.9999,
-) -> Array:
-    ep_rewards = jnp.zeros(num_episodes)
+    learning_rate: float = 0.1,
+    epsilon: float = 0.05,
+    gamma: float = 0.99,
+    total_timesteps: int = 100_000,
+    seed: int = 1,
+    logger: LoggerBase | None = None,
+) -> ArrayLike:
+    key = jax.random.key(seed)
 
-    for i in tqdm(range(num_episodes)):
-        key, subkey = random.split(key)
-        q_table, ep_reward = _q_learning_episode(
-            subkey, env, q_table, alpha, epsilon, gamma
-        )
-        ep_rewards = ep_rewards.at[i].add(ep_reward)
+    if logger is not None:
+        logger.start_new_episode()
 
-    return q_table, ep_rewards
-
-
-def _q_learning_episode(
-    key: PRNGKey,
-    env: gymnasium.Env,
-    q_table: ArrayLike,
-    alpha: float,
-    epsilon: float,
-    gamma: float = 0.9999,
-) -> float:
-    """
-    Performs a single episode rollout.
-
-    :param gamma: Discount factor.
-    :return: Episode reward.
-    """
-    ep_reward = 0
-    truncated = False
-    terminated = False
     observation, _ = env.reset()
+    steps_per_episode = 0
 
-    while not terminated and not truncated:
-        key, subkey1, subkey2 = random.split(key, 3)
+    for i in tqdm.trange(total_timesteps):
+        steps_per_episode += 1
+        key, subkey1, subkey2 = jax.random.split(key, 3)
 
         action = get_epsilon_greedy_action(
             subkey1, q_table, observation, epsilon
         )
-        # get action from policy and perform environment step
-        next_observation, reward, terminated, truncated, _ = env.step(
+
+        next_observation, reward, terminated, truncated, info = env.step(
             int(action)
         )
-        # get next action
-        next_action = get_greedy_action(subkey2, q_table, observation)
 
-        # update target policy
-        q_table = _q_learning_update(
+        next_action = get_greedy_action(subkey2, q_table, next_observation)
+
+        q_table = _update_policy(
             q_table,
             observation,
             action,
@@ -71,18 +48,24 @@ def _q_learning_episode(
             next_observation,
             next_action,
             gamma,
-            alpha,
+            terminated,
+            learning_rate,
         )
 
-        # housekeeping
-        observation = next_observation
-        ep_reward += reward
+        if terminated or truncated:
+            if logger is not None:
+                logger.record_stat("return", info["episode"]["r"], step=i)
+                logger.stop_episode(steps_per_episode)
+            observation, _ = env.reset()
+            steps_per_episode = 0
+        else:
+            observation = next_observation
 
-    return q_table, ep_reward
+    return q_table
 
 
-@jit
-def _q_learning_update(
+@jax.jit
+def _update_policy(
     q_table,
     observation,
     action,
@@ -90,11 +73,12 @@ def _q_learning_update(
     next_observation,
     next_action,
     gamma,
-    alpha,
+    terminated,
+    learning_rate,
 ):
     val = q_table[observation, action]
-    next_val = q_table[next_observation, next_action]
+    next_val = (1 - terminated) * q_table[next_observation, next_action]
     error = td_error(reward, gamma, val, next_val)
-    q_table = q_table.at[observation, action].add(alpha * error)
+    q_table = q_table.at[observation, action].add(learning_rate * error)
 
     return q_table
