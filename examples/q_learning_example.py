3fb96c08dc5675fad5cb1bc7ca99289e8ba4bcc5
Melvin Laux <laux@uni-bremen.de>
2025-05-07 17:13:46 +0200

    Update Q-Learning interface in example

--- examples/q_learning_example.py
+++ examples/q_learning_example.py
@@ -2,37 +2,35 @@ from functools import partial
 
 import gymnasium as gym
 from gymnasium.wrappers import RecordEpisodeStatistics
-from jax.random import PRNGKey
 
-from rl_blox.algorithm.q_learning import q_learning
+from rl_blox.algorithm.q_learning import train_q_learning
 from rl_blox.blox.value_policy import get_greedy_action, make_q_table
 from rl_blox.util.experiment_helper import generate_rollout
 
-NUM_EPISODES = 2000
-LEARNING_RATE = 0.05
-EPSILON = 0.05
-KEY = PRNGKey(42)
-WINDOW_SIZE = 10
+NUM_STEPS = 20_000
+LEARNING_RATE = 0.1
+EPSILON = 0.1
 ENV_NAME = "CliffWalking-v0"
 
 env = gym.make(ENV_NAME)
-env = RecordEpisodeStatistics(env, buffer_length=NUM_EPISODES)
+env = RecordEpisodeStatistics(env, buffer_length=2000)
 
 q_table = make_q_table(env)
 
-q_table, ep_rewards = q_learning(
-    KEY,
+q_table = train_q_learning(
     env,
     q_table,
-    alpha=LEARNING_RATE,
+    learning_rate=LEARNING_RATE,
     epsilon=EPSILON,
-    num_episodes=NUM_EPISODES,
+    total_timesteps=NUM_STEPS,
+    seed=42,
 )
+
 
 env.close()
 
 # create and run the final policy
-policy = partial(get_greedy_action, key=KEY, q_table=q_table)
+policy = partial(get_greedy_action, key=42, q_table=q_table)
 
 test_env = gym.make(ENV_NAME, render_mode="human")
 generate_rollout(test_env, policy)
