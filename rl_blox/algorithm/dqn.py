b3a9cbe3077714e6a665d8ccde0c5aa0a76d3a2b
Melvin Laux <melvin.laux@dfki.de>
2025-05-16 13:56:22 +0200

    Move linear schedule function to blox

--- rl_blox/algorithm/dqn.py
+++ rl_blox/algorithm/dqn.py
@@ -8,45 +8,7 @@ from tqdm import tqdm
 
 from ..blox.function_approximator.mlp import MLP
 from ..blox.replay_buffer import ReplayBuffer, Transition
-
-
-def linear_schedule(
-    total_timesteps: int,
-    start: float = 1.0,
-    end: float = 0.1,
-    fraction: float = 0.1,
-) -> jnp.ndarray:
-    """Creates a linear schedule of values for given amount of time steps.
-
-    Parameters
-    ----------
-
-    total_timesteps : int
-        Number of time steps to create the schedule for.
-    start : float, optional
-        The starting value of the schedule.
-    end : float, optional
-        The final value of the schedule.
-    fraction : float
-        The fraction of time steps in which the schedule should complete.
-
-    Returns
-    -------
-
-    schedule : jnp.ndarray
-        The schedule of values for all time steps in JAX array form.
-
-    """
-    transition_steps = int(
-        total_timesteps * fraction
-    )  # Number of steps for decay
-    schedule = jnp.ones(total_timesteps) * end  # Default value after decay
-
-    schedule.at[:transition_steps].set(
-        jnp.linspace(start, end, transition_steps)
-    )
-
-    return schedule
+from ..blox.schedules import linear_schedule
 
 
 @jax.jit
