5e1c112116d2c36efef34f98332b457f4993d9e8
Alexander Fabisch <afabisch@googlemail.com>
2025-05-18 18:50:07 +0200

    Extract stochastic_policy_gradient_pseudo_loss

--- rl_blox/blox/losses.py
+++ rl_blox/blox/losses.py
@@ -3,6 +3,8 @@ import jax.numpy as jnp
 import optax
 from flax import nnx
 
+from .function_approximator.policy_head import StochasticPolicyBase
+
 
 def mse_value_loss(
     observations: jnp.ndarray,
@@ -39,3 +41,61 @@ def mse_value_loss(
     values = v(observations).squeeze()  # squeeze Nx1-D -> N-D
     chex.assert_equal_shape((values, v_target))
     return optax.l2_loss(predictions=values, targets=v_target).mean()
+
+
+def stochastic_policy_gradient_pseudo_loss(
+    observation: jnp.ndarray,
+    action: jnp.ndarray,
+    weight: jnp.ndarray,
+    policy: StochasticPolicyBase,
+) -> jnp.ndarray:
+    r"""Pseudo loss for the policy gradient.
+
+    For a given stochastic policy :math:`\pi(a|o)`, observations :math:`o_i`,
+    actions :math:`a_i`, and corresponding weights :math:`w_i`, the pseudo loss
+    is defined as
+
+    .. math::
+
+        \mathcal{L}(\pi)
+        =
+        -\frac{1}{N} \sum_{i=1}^{N} w_i \log \pi(a_i|o_i).
+
+    The calculation of weights depends on the specific algorithm. We take the
+    negative value of the pseudo loss, because we want to perform gradient
+    ascent with the policy gradient, but we use a gradient descent optimizer.
+
+    Parameters
+    ----------
+    observation : array, shape (n_samples, n_observation_features)
+        Observations.
+
+    action : array, shape (n_samples, n_action_features)
+        Actions.
+
+    weight : array, shape (n_samples,)
+        Weights for the policy gradient.
+
+    policy : nnx.Module
+        Policy :math:`\pi(a|o)`. We have to be able to compute
+        :math:`\log \pi(a|o)` with
+        `policy.log_probability(observations, actions)`.
+
+    Returns
+    -------
+    loss : float
+        Pseudo loss for the policy gradient.
+
+    See Also
+    --------
+    .algorithm.reinforce.reinforce_gradient
+        Uses this function to calculate the REINFORCE policy gradient.
+
+    .algorithm.actor_critic.actor_critic_policy_gradient
+        Uses this function to calculate the actor-critic policy gradient.
+    """
+    logp = policy.log_probability(observation, action)
+    chex.assert_equal_shape((weight, logp))
+    return -jnp.mean(
+        weight * logp
+    )  # - to perform gradient ascent with a minimizer
