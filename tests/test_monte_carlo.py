bae3ceb09abfe19c2f61e726d8dc7229a0bb088b
Melvin Laux <melvin.laux@dfki.de>
2025-05-19 18:42:50 +0200

    Reactivate MC test

--- tests/test_monte_carlo.py
+++ tests/test_monte_carlo.py
@@ -1,10 +1,11 @@
 import gymnasium as gym
-import pytest
+
+from rl_blox.algorithm.monte_carlo import train_monte_carlo
+from rl_blox.blox.value_policy import make_q_table
 
 
-@pytest.mark.skip
 def test_monte_carlo():
-    env = gym.make("FrozenLake-v1")
-    mc = MonteCarlo(env, 0.1, "first_visit")
-    mc.train(1)
+    env = gym.make("CliffWalking-v0")
+    q_table = make_q_table(env)
+    _ = train_monte_carlo(env, q_table, total_timesteps=10)
     env.close()
