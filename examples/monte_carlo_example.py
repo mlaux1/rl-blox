7673733c46e849c1ff65b15a1e31451bbc79e35b
Melvin Laux <melvin.laux@dfki.de>
2025-05-19 12:31:29 +0200

    Use new functional interface for example

--- examples/monte_carlo_example.py
+++ examples/monte_carlo_example.py
@@ -1,12 +1,29 @@
+from functools import partial
+
 import gymnasium as gym
+from gymnasium.wrappers import RecordEpisodeStatistics
 
-from rl_blox.algorithm.monte_carlo import MonteCarlo
+from rl_blox.algorithm.monte_carlo import train_monte_carlo
+from rl_blox.blox.value_policy import get_greedy_action, make_q_table
+from rl_blox.util.experiment_helper import generate_rollout
 
-train_env = gym.make(
-    "FrozenLake-v1", render_mode="human", desc=["SFFH", "FFFF", "FFFF", "FFFG"]
-)
+ENV_NAME = "CliffWalking-v0"
+NUM_STEPS = 20_000
 
-qmc = MonteCarlo(train_env, 0.1, "first_visit")
-qmc.train(10)
+
+train_env = gym.make(ENV_NAME)
+train_env = RecordEpisodeStatistics(train_env)
+
+q_table = make_q_table(train_env)
+
+q_table, _ = train_monte_carlo(train_env, q_table, total_timesteps=NUM_STEPS)
 
 train_env.close()
+
+print(q_table)
+
+policy = partial(get_greedy_action, key=42, q_table=q_table)
+
+test_env = gym.make(ENV_NAME, render_mode="human")
+generate_rollout(test_env, policy)
+test_env.close()
