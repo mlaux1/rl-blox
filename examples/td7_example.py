d6bc57d9bed7e047e613b25d27e237cafa1d09d7
Alexander Fabisch <afabisch@googlemail.com>
2025-06-14 17:51:08 +0200

    Sow final policy

--- examples/td7_example.py
+++ examples/td7_example.py
@@ -5,7 +5,7 @@ import numpy as np
 from rl_blox.algorithm.td7 import create_td7_state, train_td7
 from rl_blox.logging.logger import AIMLogger, LoggerList, StandardLogger
 
-env_name = "Hopper-v5"
+env_name = "Hopper-v5"  # "Pendulum-v1", "Hopper-v5"
 env = gym.make(env_name)
 
 seed = 1
@@ -24,11 +24,11 @@ hparams_algorithm = dict(
     exploration_noise=0.1,
     target_policy_noise=0.2,
     noise_clip=0.5,
-    total_timesteps=1_000_000,
+    total_timesteps=15_000 if env_name == "Pendulum-v1" else 1_000_000,
     buffer_size=1_000_000,
-    learning_starts=25_000,
+    learning_starts=5_000 if env_name == "Pendulum-v1" else 25_000,
     batch_size=256,
-    use_checkpoints=False,
+    use_checkpoints=True,
     seed=seed,
 )
 
@@ -50,7 +50,7 @@ logger.define_experiment(
 
 td7_state = create_td7_state(env, **hparams_models)
 
-td3_result = train_td7(
+result = train_td7(
     env,
     embedding=td7_state.embedding,
     embedding_optimizer=td7_state.embedding_optimizer,
@@ -62,4 +62,21 @@ td3_result = train_td7(
     **hparams_algorithm,
 )
 env.close()
-# TODO
+
+# Evaluation
+env = gym.make(env_name, render_mode="human")
+while True:
+    done = False
+    infos = {}
+    obs, _ = env.reset()
+    while not done:
+        obs = jnp.asarray(obs)
+        zs = result.embedding.state_embedding(obs)
+        action = result.actor(obs, zs=zs)
+        next_obs, reward, termination, truncation, infos = env.step(np.asarray(action))
+        done = termination or truncation
+        zsa = result.embedding.state_action_embedding(jnp.concatenate((zs, action), axis=-1))
+        q_value = result.critic(jnp.concatenate((obs, action)), zs=zs, zsa=zsa)
+        if verbose:
+            print(f"{q_value=}")
+        obs = np.asarray(next_obs)
