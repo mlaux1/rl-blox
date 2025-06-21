96e2741a59832cb87f4239196dcd80f1079c0e0c
Alexander Fabisch <afabisch@googlemail.com>
2025-06-21 22:32:02 +0200

    Log q bounds in each step

--- rl_blox/algorithm/td7.py
+++ rl_blox/algorithm/td7.py
@@ -1128,12 +1128,11 @@ def train_td7(
                         epochs["fixed_embedding_target"] = (
                             fixed_embedding_target
                         )
-                        # TODO target and current value will always be the same if logged like this...
-                        metrics.update(value_clipping_state.__dict__)
 
                 if logger is not None:
                     metrics["embedding loss"] = embedding_loss_value
                     metrics["q loss"] = q_loss_value
+                    metrics.update(value_clipping_state.__dict__)
                     epochs["embedding"] = actor
                     epochs["q"] = critic
 
