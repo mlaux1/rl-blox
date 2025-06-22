bd0fef6566ba0aa63962f5bb680df589e04c66bf
Alexander Fabisch <afabisch@googlemail.com>
2025-06-22 15:17:22 +0200

    Document checkpointing

--- rl_blox/blox/checkpointing.py
+++ rl_blox/blox/checkpointing.py
@@ -3,11 +3,17 @@ import dataclasses
 
 @dataclasses.dataclass
 class CheckpointState:
+    """State of checkpoint monitoring."""
     episodes_since_udpate: int = 0
+    """Number of assessment episodes since last actor update."""
     timesteps_since_upate: int = 0
+    """Number of environment steps since last actor update."""
     max_episodes_before_update: int = 1
+    """Maximum number of episodes allowed before next actor update."""
     min_return: float = 1e8
+    """Minimum return observed for current actor."""
     best_min_return: float = -1e8
+    """Highest minimum return observed for any previous actor."""
 
 
 def maybe_train_and_checkpoint(
@@ -24,36 +30,28 @@ def maybe_train_and_checkpoint(
     The function will determine if a checkpoint should be saved and for how many
     epochs we should train.
 
-    The ideal performance measure of a policy is the average return in as many
-    episodes as possible. However, it is necessary to reduce the number of
-    evaluation episodes to improve sample efficiency. We use the minimum
-    performance to assess unstable policies with a low number of episodes as
-    poorly performing policies do not waste additional assessment episodes and
-    training can resume when the performance in any episode falls below the
-    checkpoint performance. This idea is first used in TD7 [1]_.
-
     Parameters
     ----------
     checkpoint_state : CheckpointState
         State of checkpoint monitoring.
 
     steps_per_episode : int
-        Steps taken in the last episode.
+        Number of steps in the previous episode that just finished.
 
     episode_return : float
-        Return of the last episode.
+        Return of the previous episode that just finished.
 
     epoch : int
         Training epoch counter.
 
     reset_weight : float
-        Criteria reset weight.
+        Configuration: criteria reset weight.
 
     max_episodes_when_checkpointing : int
-        Maximum number of assessment episodes.
+        Configuration: maximum number of assessment episodes.
 
     steps_before_checkpointing : int
-        Maximum number of timesteps before checkpointing.
+        Configuration: maximum number of timesteps before checkpointing.
 
     Returns
     -------
@@ -63,6 +61,17 @@ def maybe_train_and_checkpoint(
     training_steps : int
         Number of training epochs.
 
+    Notes
+    -----
+
+    The ideal performance measure of a policy is the average return in as many
+    episodes as possible. However, it is necessary to reduce the number of
+    evaluation episodes to improve sample efficiency. We use the minimum
+    performance to assess unstable policies with a low number of episodes as
+    poorly performing policies do not waste additional assessment episodes and
+    training can resume when the performance in any episode falls below the
+    checkpoint performance. This idea is first used in TD7 [1]_.
+
     References
     ----------
     .. [1] Fujimoto, S., Chang, W.D., Smith, E., Gu, S., Precup, D., Meger, D.
@@ -70,6 +79,29 @@ def maybe_train_and_checkpoint(
        Reinforcement Learning. In Advances in Neural Information Processing
        Systems 36, pp. 61573-61624. Available from
        https://proceedings.neurips.cc/paper_files/paper/2023/hash/c20ac0df6c213db6d3a930fe9c7296c8-Abstract-Conference.html
+
+    Examples
+    --------
+
+    The procedure works as follows:
+
+    .. code-block:: python
+
+        if terminated or truncated:  # episode done
+            update_checkpoint, training_steps = maybe_train_and_checkpoint(
+                checkpoint_state,
+                steps_per_episode,  # number of steps in the last episode
+                episode_return,  # accumulated reward of the last episode
+                epoch,
+                reset_weight,
+                max_episodes_when_checkpointing,
+                steps_before_checkpointing,
+            )
+            if update_checkpoint:
+                update_actor_checkpoint(...)
+            for _ in range(training_steps):
+                train(...)
+                epoch += 1
     """
     checkpoint_state.episodes_since_udpate += 1
     checkpoint_state.timesteps_since_upate += steps_per_episode
@@ -81,18 +113,21 @@ def maybe_train_and_checkpoint(
     training_steps = 0
 
     if checkpoint_state.min_return < checkpoint_state.best_min_return:
-        # End evaluation of current policy early
+        # We know that the current actor is not better than the current
+        # checkpoint. End evaluation of current actor early.
         training_steps = checkpoint_state.timesteps_since_upate
     elif (
         checkpoint_state.episodes_since_udpate
         == checkpoint_state.max_episodes_before_update
     ):
-        # Update checkpoint and train
+        # The current actor seems to be better, and we exhausted our
+        # assessment budget. Update actor checkpoint and train actor.
         checkpoint_state.best_min_return = checkpoint_state.min_return
         update_checkpoint = True
         training_steps = checkpoint_state.timesteps_since_upate
 
     if training_steps > 0:
+        # Update checkpoint monitoring state.
         for i in range(checkpoint_state.timesteps_since_upate):
             if epoch + i + 1 == steps_before_checkpointing:
                 checkpoint_state.best_min_return *= reset_weight
