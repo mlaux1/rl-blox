d6e7ebc1da48441b65c5ec539ea8750e1fcf1c63
Melvin Laux <melvin.laux@dfki.de>
2025-06-27 21:35:26 +0200

    Create fixtures for all test envs

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,3 +7,24 @@ def tabular_test_env():
     env = gym.make("CliffWalking-v1")
     yield env
     env.close()
+
+
+@pytest.fixture(scope="session")
+def cart_pole_env():
+    env = gym.make("CartPole-v1")
+    yield env
+    env.close()
+
+
+@pytest.fixture(scope="session")
+def inverted_pendulum_env():
+    env = gym.make("InvertedPendulum-v5")
+    yield env
+    env.close()
+
+
+@pytest.fixture(scope="session")
+def pendulum_env():
+    env = gym.make("Pendulum-v1")
+    yield env
+    env.close()
