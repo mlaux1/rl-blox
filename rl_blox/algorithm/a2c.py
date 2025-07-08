9ee85c3a0bf00094544541544aafbf2993a4a879
Melvin Laux <melvin.laux@dfki.de>
2025-07-08 12:21:27 +0200

    Use vec_env for a2c

--- rl_blox/algorithm/a2c.py
+++ rl_blox/algorithm/a2c.py
@@ -1,23 +1,19 @@
 import jax
 import numpy as np
 from flax import nnx
+from gymnasium.vector import VectorEnv
 from tqdm import trange
 
 from ..logging.logger import LoggerBase
 
 
 def train_a2c(
-    env,
-    policy: nnx.Module,
-    policy_optimizer,
-    value_function: nnx.Module,
-    vf_optimizer,
-    learning_rate,
+    envs: VectorEnv,
     seed: int = 0,
     policy_gradient_steps: int = 1,
     vf_gradient_steps: int = 1,
     gamma: float = 0.9999,
-    total_timesteps: float = 1e6,
+    total_timesteps: int = 100_000,
     logger: LoggerBase | None = None,
 ) -> tuple[nnx.Module, nnx.Module]:
     """Advantage Actor-Critic (A2C)
@@ -32,12 +28,15 @@ def train_a2c(
     rng = np.random.default_rng(seed)
 
     # initialise stuff
+    obs, _ = envs.reset()
 
     for i in trange(total_timesteps):
-        print("Stepping")
-
         # for each episode
         # act and collect transitions in each env until termination
+        actions = envs.action_space.sample()
+        observations, rewards, terminations, truncations, infos = envs.step(
+            actions
+        )
 
         # backwards through gathered trajectories
         # compute advantage estimates and accumulate gradients
