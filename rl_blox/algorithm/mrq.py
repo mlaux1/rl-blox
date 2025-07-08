4da1ced3dad5a2ba29417876e5d56c2328a53073
Alexander Fabisch <afabisch@googlemail.com>
2025-07-08 22:53:04 +0200

    Add layer norm MLP

--- rl_blox/algorithm/mrq.py
+++ rl_blox/algorithm/mrq.py
@@ -1,7 +1,8 @@
 import dataclasses
-from collections import namedtuple
+from collections import Callable, namedtuple
 from functools import partial
 
+import chex
 import gymnasium as gym
 import jax.numpy as jnp
 import jax.random
@@ -33,6 +34,87 @@ class EpisodicReplayBuffer:
         rng: np.random.Generator,
     ) -> tuple[jnp.ndarray]:
         raise NotImplementedError()
+
+
+class LayerNormMLP(nnx.Module):
+    """Multilayer Perceptron.
+
+    Parameters
+    ----------
+    n_features : int
+        Number of features.
+
+    n_outputs : int
+        Number of output components.
+
+    hidden_nodes : list
+        Numbers of hidden nodes of the MLP.
+
+    activation : str
+        Activation function. Has to be the name of a function defined in the
+        flax.nnx module.
+
+    rngs : nnx.Rngs
+        Random number generator.
+    """
+
+    n_outputs: int
+    """Number of output components."""
+
+    activation: Callable[[jnp.ndarray], jnp.ndarray]
+    """Activation function."""
+
+    hidden_layers: list[nnx.Linear]
+    """Hidden layers."""
+
+    output_layer: nnx.Linear
+    """Output layer."""
+
+    def __init__(
+        self,
+        n_features: int,
+        n_outputs: int,
+        hidden_nodes: list[int],
+        activation: str,
+        rngs: nnx.Rngs,
+    ):
+        chex.assert_scalar_positive(n_features)
+        chex.assert_scalar_positive(n_outputs)
+
+        self.n_outputs = n_outputs
+        self.activation = getattr(nnx, activation)
+
+        self.hidden_layers = []
+        self.layer_norms = []
+        n_in = n_features
+        for n_out in hidden_nodes:
+            self.hidden_layers.append(nnx.Linear(n_in, n_out, rngs=rngs))
+            self.layer_norms.append(
+                nnx.LayerNorm(num_features=n_out, rngs=rngs)
+            )
+            n_in = n_out
+
+        self.output_layer = nnx.Linear(n_in, n_outputs, rngs=rngs)
+
+    def __call__(self, x: jnp.ndarray) -> jnp.ndarray:
+        for layer, norm in zip(
+            self.hidden_layers, self.layer_norms, strict=True
+        ):
+            x = self.activation(norm(layer(x)))
+        return self.output_layer(x)
+
+
+def create_mrq_state(
+    env: gym.Env[gym.spaces.Box, gym.spaces.Box],
+    policy_hidden_nodes: list[int] | tuple[int] = (256, 256),
+    policy_activation: str = "relu",
+    policy_learning_rate: float = 3e-4,
+    q_hidden_nodes: list[int] | tuple[int] = (256, 256),
+    q_activation: str = "elu",
+    q_learning_rate: float = 3e-4,
+    seed: int = 0,
+):
+    return namedtuple("MRQState", [])()
 
 
 def train_mrq(
