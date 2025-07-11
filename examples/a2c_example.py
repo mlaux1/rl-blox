cb10c6bd6a00333710968d2304818121dd699344
Melvin Laux <laux@uni-bremen.de>
2025-07-11 18:41:07 +0200

    Restart from actor-critic

--- examples/a2c_example.py
+++ examples/a2c_example.py
@@ -1,10 +1,70 @@
 import gymnasium as gym
+import jax.numpy as jnp
+import numpy as np
 
 from rl_blox.algorithm.a2c import train_a2c
+from rl_blox.algorithm.reinforce import create_policy_gradient_continuous_state
+from rl_blox.logging.logger import AIMLogger, LoggerList, StandardLogger
 
-env_name = "CartPole-v1"
-train_envs = gym.make_vec(env_name, 2)
+# env_name = "Pendulum-v1"
+# env_name = "HalfCheetah-v4"
+env_name = "InvertedPendulum-v5"
+env = gym.make(env_name)
 seed = 42
+env.reset(seed=seed)
 
+hparams_model = dict(
+    policy_shared_head=True,
+    policy_hidden_nodes=[32, 32],
+    policy_learning_rate=3e-4,
+    value_network_hidden_nodes=[128, 128],
+    value_network_learning_rate=1e-2,
+    seed=seed,
+)
+hparams_algorithm = dict(
+    policy_gradient_steps=5,
+    value_gradient_steps=5,
+    total_timesteps=900_000,
+    gamma=0.99,
+    steps_per_update=5_000,
+    train_after_episode=False,
+    seed=seed,
+)
 
-train_a2c(train_envs, None, None)
+logger = LoggerList([StandardLogger(verbose=2), AIMLogger()])
+logger.define_experiment(
+    env_name=env_name,
+    algorithm_name="A2C",
+    hparams=hparams_model | hparams_algorithm,
+)
+logger.define_checkpoint_frequency("value_function", 10)
+
+ac_state = create_policy_gradient_continuous_state(env, **hparams_model)
+
+train_a2c(
+    env,
+    ac_state.policy,
+    ac_state.policy_optimizer,
+    ac_state.value_function,
+    ac_state.value_function_optimizer,
+    **hparams_algorithm,
+    logger=logger,
+)
+env.close()
+
+# Evaluation
+env = gym.make(env_name, render_mode="human")
+env = gym.wrappers.RecordEpisodeStatistics(env)
+while True:
+    done = False
+    infos = {}
+    obs, _ = env.reset()
+    while not done:
+        action = np.asarray(ac_state.policy(jnp.asarray(obs)))
+        next_obs, reward, termination, truncation, infos = env.step(action)
+        done = termination or truncation
+        obs = np.asarray(next_obs)
+    if "final_info" in infos:
+        for info in infos["final_info"]:
+            print(f"episodic_return={info['episode']['r']}")
+            break
--- rl_blox/algorithm/a2c.py
+++ rl_blox/algorithm/a2c.py
@@ -1,63 +1,217 @@
+from functools import partial
+
+import gymnasium as gym
 import jax
-import numpy as np
+import jax.numpy as jnp
+import tqdm
 from flax import nnx
-from gymnasium.vector import VectorEnv
-from tqdm import trange
 
+from ..blox.function_approximator.mlp import MLP
+from ..blox.function_approximator.policy_head import StochasticPolicyBase
+from ..blox.losses import stochastic_policy_gradient_pseudo_loss
 from ..logging.logger import LoggerBase
+from .reinforce import sample_trajectories, train_value_function
 
 
-def train_a2c(
-    envs: VectorEnv,
-    actor: nnx.Module,
-    critic: nnx.Module,
-    t_max: int = 100,
-    seed: int = 0,
-    policy_gradient_steps: int = 1,
-    vf_gradient_steps: int = 1,
-    gamma: float = 0.9999,
-    total_timesteps: int = 100_000,
-    logger: LoggerBase | None = None,
-) -> tuple[nnx.Module, nnx.Module]:
-    """Advantage Actor-Critic (A2C)
+def actor_critic_policy_gradient(
+    policy: StochasticPolicyBase,
+    value_function: nnx.Module,
+    observations: jnp.ndarray,
+    actions: jnp.ndarray,
+    next_observations: jnp.ndarray,
+    rewards: jnp.ndarray,
+    gamma_discount: jnp.ndarray,
+    gamma: float,
+) -> jnp.ndarray:
+    r"""Actor-critic policy gradient.
 
     Parameters
     ----------
-    envs : VectorEnv
-        The environments to train in.
-    actor : nnx.Module
-        The policy network.
-    critic : nnx.Module
-        The value function estimation network.
+    policy
+        Probabilistic policy that we want to update and has been used for
+        exploration.
+    value_function
+        Estimated value function.
+    observations
+        Samples that were collected with the policy.
+    actions
+        Samples that were collected with the policy.
+    next_observations
+        Samples that were collected with the policy.
+    rewards
+        Samples that were collected with the policy.
+    gamma_discount
+        Discounting for individual steps of the episode.
+    gamma
+        Discount factor.
+
+    Returns
+    -------
+    loss
+        Actor-critic policy gradient pseudo loss.
+    grad
+        Actor-critic policy gradient.
+
+    See Also
+    --------
+    .blox.losses.stochastic_policy_gradient_pseudo_loss
+        The pseudo loss that is used to compute the policy gradient. As
+        weights for the pseudo loss we use the TD error
+        :math:`\delta_t = r_t + \gamma v(o_{t+1}) - v(o_t)` multiplied by the
+        discounting factor for the step of the episode.
     """
+    v = value_function(observations).squeeze()
+    v_next = value_function(next_observations).squeeze()
+    td_bootstrap_estimate = rewards + gamma * v_next - v
+    weights = gamma_discount * td_bootstrap_estimate
 
+    return nnx.value_and_grad(
+        stochastic_policy_gradient_pseudo_loss, argnums=3
+    )(observations, actions, weights, policy)
+
+
+def train_a2c(
+    env: gym.Env,
+    policy: StochasticPolicyBase,
+    policy_optimizer: nnx.Optimizer,
+    value_function: MLP,
+    value_function_optimizer: nnx.Optimizer,
+    seed: int = 0,
+    policy_gradient_steps: int = 1,
+    value_gradient_steps: int = 1,
+    total_timesteps: int = 1_000_000,
+    gamma: float = 1.0,
+    steps_per_update: int = 1_000,
+    train_after_episode: bool = False,
+    logger: LoggerBase | None = None,
+):
+    """Train with actor-critic.
+
+    Parameters
+    ----------
+    env : gym.Env
+        Environment.
+
+    policy : nnx.Module
+        Probabilistic policy network. Maps observations to probability
+        distribution over actions.
+
+    policy_optimizer : nnx.Optimizer
+        Optimizer for policy network.
+
+    value_function : nnx.Module or None, optional
+        Policy network. Maps observations to expected returns.
+
+    value_function_optimizer : nnx.Optimizer or None, optional
+        Optimizer for value function network.
+
+    seed : int, optional
+        Seed for random number generation.
+
+    policy_gradient_steps : int, optional
+        Number of gradient descent steps for the policy network.
+
+    value_gradient_steps : int, optional
+        Number of gradient descent steps for the value network.
+
+    total_timesteps
+        Total timesteps of the experiments.
+
+    gamma : float, optional
+        Discount factor for rewards.
+
+    steps_per_update : int, optional
+        Number of samples to collect before updating the policy. Alternatively
+        you can train after each episode.
+
+    train_after_episode : bool, optional
+        Train after each episode. Alternatively you can train after collecting
+        a certain number of samples.
+
+    key : jnp.ndarray, optional
+        Pseudo random number generator key for action sampling.
+
+    logger : logger.LoggerBase, optional
+        Experiment logger.
+    """
     key = jax.random.key(seed)
-    rng = np.random.default_rng(seed)
+    progress = tqdm.tqdm(total=total_timesteps)
+    step = 0
+    while step < total_timesteps:
+        key, skey = jax.random.split(key, 2)
+        dataset = sample_trajectories(
+            env, policy, skey, logger, train_after_episode, steps_per_update
+        )
+        step += len(dataset)
+        progress.update(len(dataset))
 
-    # initialise stuff
-    obs, _ = envs.reset()
-    terminations = [np.array([False, False])]
-    truncations = [np.array([False, False])]
-    rewards = []
-    actions = []
-    observations = []
+        observations, actions, next_observations, returns, gamma_discount = (
+            dataset.prepare_policy_gradient_dataset(env.action_space, gamma)
+        )
+        rewards = jnp.hstack(
+            [
+                jnp.hstack([r for _, _, _, r in episode])
+                for episode in dataset.episodes
+            ]
+        )
 
-    for i in trange(t_max):
-        # for each episode
-        # act and collect transitions in each env until termination
-        acts = envs.action_space.sample()
-        obs, rews, terms, truncs, _ = envs.step(acts)
-        observations.append(obs)
-        actions.append(acts)
-        rewards.append(rews)
-        terminations.append(np.logical_or(terms, terminations[-1]))
-        truncations.append(np.logical_and(truncs, truncations[-1]))
+        p_loss = train_policy_actor_critic(
+            policy,
+            policy_optimizer,
+            policy_gradient_steps,
+            value_function,
+            observations,
+            actions,
+            next_observations,
+            rewards,
+            gamma_discount,
+            gamma,
+        )
+        if logger is not None:
+            logger.record_stat(
+                "policy loss", p_loss, episode=logger.n_episodes - 1
+            )
+            logger.record_epoch("policy", policy)
 
-        # backwards through gathered trajectories
-        # compute advantage estimates and accumulate gradients
-    for i in range(t_max - 1, 0, -1):
-        print(actions[i])
+        v_loss = train_value_function(
+            value_function,
+            value_function_optimizer,
+            value_gradient_steps,
+            observations,
+            returns,
+        )
+        if logger is not None:
+            logger.record_stat(
+                "value function loss", v_loss, episode=logger.n_episodes - 1
+            )
+            logger.record_epoch("value_function", value_function)
+    progress.close()
 
-        # perform policy and value function update
 
-    return
+@partial(nnx.jit, static_argnames=["policy_gradient_steps", "gamma"])
+def train_policy_actor_critic(
+    policy,
+    policy_optimizer,
+    policy_gradient_steps,
+    value_function,
+    observations,
+    actions,
+    next_observations,
+    rewards,
+    gamma_discount,
+    gamma,
+):
+    p_loss = 0.0
+    for _ in range(policy_gradient_steps):
+        p_loss, p_grad = actor_critic_policy_gradient(
+            policy,
+            value_function,
+            observations,
+            actions,
+            next_observations,
+            rewards,
+            gamma_discount,
+            gamma,
+        )
+        policy_optimizer.update(p_grad)
+    return p_loss
