89db29557e30d4dbfa966e105f53b97246b3feb5
Melvin Laux <laux@uni-bremen.de>
2025-07-23 15:53:26 +0200

    Add initial version of UTS-SAC

---
+++ rl_blox/algorithm/multi_task/uts_sac.py
@@ -0,0 +1,66 @@
+import random
+
+import gymnasium as gym
+from flax import nnx
+
+from ...blox.double_qnet import ContinuousClippedDoubleQNet
+from ...blox.function_approximator.policy_head import StochasticPolicyBase
+from ...blox.replay_buffer import ReplayBuffer
+from ..sac import EntropyControl, train_sac
+
+
+def train_uts_sac(
+    envs: list[gym.Env],
+    policy: StochasticPolicyBase,
+    policy_optimizer: nnx.Opimizer,
+    q_net: ContinuousClippedDoubleQNet,
+    q_optimizer: nnx.Optimizer,
+    time_steps_per_epoch: int,
+    epochs: int,
+    seed: int = 1,
+) -> tuple[
+    nnx.Module,
+    nnx.Optimizer,
+    nnx.Module,
+    nnx.Module,
+    nnx.Optimizer,
+    EntropyControl,
+    ReplayBuffer,
+]:
+    replay_buffer = None
+    q_target = None
+    entropy_control = None
+
+    for i in range(epochs):
+        env = random.choice(envs)
+        (
+            policy,
+            policy_optimizer,
+            q_net,
+            q_target,
+            q_optimizer,
+            entropy_control,
+            replay_buffer,
+        ) = train_sac(
+            env,
+            policy,
+            policy_optimizer,
+            q_net,
+            q_optimizer,
+            seed=seed,
+            total_timesteps=time_steps_per_epoch,
+            replay_buffer=replay_buffer,
+            q_target=q_target,
+            entropy_control=entropy_control,
+        )
+        print(f"Epoche {i} complete.")
+
+    return (
+        policy,
+        policy_optimizer,
+        q_net,
+        q_target,
+        q_optimizer,
+        entropy_control,
+        replay_buffer,
+    )
